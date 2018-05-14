5feb5e64b4bf194cb06ec403635e565c92a0920f
joybeanx <ijoybeansang@gmail.com>
2018-05-14 11:27:08 +0800

    Reverse Integer

---
+++ src/main/java/com/joybean/oj/leetcode/PalindromeNumber.java
@@ -0,0 +1,41 @@
+package com.joybean.oj.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/palindrome-number/description/">Palindrome Number</a>
+ *
+ * @author Joybean
+ */
+public class PalindromeNumber {
+
+    public static boolean isPalindrome1(int x) {
+        String str = String.valueOf(x);
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome2(int x) {
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
