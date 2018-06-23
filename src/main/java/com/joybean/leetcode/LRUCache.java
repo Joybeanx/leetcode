1ceeb65bc247f35ca897cd8a996e39a9cc2b9f7b
joybeanx <ijoybeansang@gmail.com>
2018-06-23 11:01:53 +0800

    LRUCache & PlusOne

---
+++ src/main/java/com/joybean/leetcode/LRUCache.java
@@ -0,0 +1,39 @@
+package com.joybean.leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/lru-cache/description/">LRU Cache</a>
+ *
+ * @author Joybean
+ */
+public class LRUCache {
+    private final LinkedHashMap<Integer, Integer> cacheHolder;
+
+    static class LRULinkedHashMap extends LinkedHashMap<Integer, Integer> {
+        private int capacity;
+
+        public LRULinkedHashMap(int capacity) {
+            super(capacity, 0.75f, true);
+            this.capacity = capacity;
+        }
+
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return this.size() > capacity;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        cacheHolder = new LRULinkedHashMap(capacity);
+    }
+
+    public int get(int key) {
+        //Use getOrDefault instead of if clause
+        return cacheHolder.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cacheHolder.put(key, value);
+    }
+}
---
+++ src/main/java/com/joybean/leetcode/PlusOne.java
@@ -0,0 +1,66 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/plus-one/description/">Plus One</a>
+ *
+ * @author Joybean
+ */
+public class PlusOne {
+    /**
+     * Keep original parameter unchanged,but less efficient
+     *
+     * @param digits
+     * @return
+     */
+    public static int[] plusOne1(int[] digits) {
+        int i = digits.length - 1;
+        boolean carry = true;
+        int[] result;
+        int[] arr = new int[digits.length];
+        do {
+            int current = digits[i];
+            if (carry) {
+                current += 1;
+                if (carry = (current > 9)) {
+                    arr[i] = 0;
+                } else {
+                    arr[i] = current;
+                }
+            } else {
+                arr[i] = current;
+            }
+            i--;
+        } while (i >= 0);
+        if (carry) {
+            result = new int[digits.length + 1];
+            result[0] = 1;
+            //It's unnecessary to copy array
+            //System.arraycopy(arr, 0, result, 1, digits.length);
+        } else {
+            result = arr;
+        }
+        return result;
+    }
+
+    /**
+     * Cool solution but it modifys original parameter
+     *
+     * @param digits
+     * @return
+     */
+    public static int[] plusOne2(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
