7cf65f329a3e2b0554b0883ea7e5f57356fd14ee
joybeanx <ijoybeansang@gmail.com>
2019-03-08 01:33:42 +0800

    New solutions of Minimum Window Substring

--- src/main/java/com/joybean/leetcode/MinimumWindowSubstring.java
+++ src/main/java/com/joybean/leetcode/MinimumWindowSubstring.java
@@ -2,7 +2,9 @@ package com.joybean.leetcode;
 
 import java.nio.CharBuffer;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,8 +33,7 @@ public class MinimumWindowSubstring {
                 f = true;
             }
             if (f) {
-                left--;
-                String tmp = s.substring(left, right);
+                String tmp = s.substring(left - 1, right);
                 minWindow = tmp.length() > minWindow.length() && !"".equals(minWindow) ? minWindow : tmp;
             }
         }
@@ -56,5 +57,132 @@ public class MinimumWindowSubstring {
             return false;
         }
         return true;
+    }
+
+    /**
+     * Two HashMap
+     * @param s
+     * @param t
+     * @return
+     */
+    public String minWindow2(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+
+        // Dictionary which keeps a count of all the unique characters in t.
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
+
+        // Number of unique characters in t, which need to be present in the desired window.
+        int required = dictT.size();
+
+        // Left and Right pointer
+        int l = 0, r = 0;
+
+        // formed is used to keep track of how many unique characters in t
+        // are present in the current window in its desired frequency.
+        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
+        // Thus formed would be = 3 when all these conditions are met.
+        int formed = 0;
+
+        // Dictionary which keeps a count of all the unique characters in the current window.
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+        // ans list of the form (window length, left, right)
+        int[] ans = {-1, 0, 0};
+
+        while (r < s.length()) {
+            // Add one character from the right to the window
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            // If the frequency of the current character added equals to the
+            // desired count in t then increment the formed count by 1.
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                // Save the smallest window until now.
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                // The character at the position pointed by the
+                // `Left` pointer is no longer a part of the window.
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+
+                // Move the left pointer ahead, this would help to look for a new window.
+                l++;
+            }
+
+            // Keep expanding the window once we are done contracting.
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
+
+    /**
+     * One HashMap
+     * @param s
+     * @param t
+     * @return
+     */
+    public static String minWindow3(String s, String t) {
+        if (s == null || s.length() < t.length() || s.length() == 0) {
+            return "";
+        }
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (char c : t.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        int left = 0;
+        int minLeft = 0;
+        int minLen = s.length() + 1;
+        int count = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
+                if (map.get(s.charAt(right)) >= 0) {
+                    count++;
+                }
+                while (count == t.length()) {
+                    if (right - left + 1 < minLen) {
+                        minLeft = left;
+                        minLen = right - left + 1;
+                    }
+                    if (map.containsKey(s.charAt(left))) {
+                        map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                        if (map.get(s.charAt(left)) > 0) {
+                            count--;
+                        }
+                    }
+                    left++;
+                }
+            }
+        }
+        if (minLen > s.length()) {
+            return "";
+        }
+
+        return s.substring(minLeft, minLeft + minLen);
     }
 }
