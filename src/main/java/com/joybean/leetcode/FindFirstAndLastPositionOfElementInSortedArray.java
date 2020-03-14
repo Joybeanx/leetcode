3b5c18e2e825de27089e3f776708e793ff6e8c8f
joybean <jianbing.sjb@alibaba-inc.com>
2020-03-14 18:08:06 +0800

    update

---
+++ src/main/java/com/joybean/leetcode/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,44 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/">Find First and Last
+ * Position of Element in Sorted Array</a>
+ *
+ * @author Joybean
+ */
+public class FindFirstAndLastPositionOfElementInSortedArray {
+
+    public static int[] searchRange1(int[] nums, int target) {
+        int idx = getTargetIndex(nums, target, 0, nums.length - 1);
+        int i = idx;
+        int j = idx;
+        if (idx != -1) {
+            while (i - 1 >= 0 && nums[i - 1] == target) {
+                i--;
+            }
+            while (j + 1 < nums.length && nums[j + 1] == target) {
+                j++;
+            }
+        }
+        return new int[] {i, j};
+    }
+
+    public static int getTargetIndex(int[] nums, int target, int start, int end) {
+        if (start >= nums.length || end >= nums.length || target > nums[end] || target < nums[start]) {
+            return -1;
+        }
+        if (target == nums[start]) {
+            return start;
+        }
+        if (target == nums[end]) {
+            return end;
+        }
+        int mi = (start + end) / 2;
+        int targetIdx;
+        if ((targetIdx = getTargetIndex(nums, target, start, mi)) != -1) {
+            return targetIdx;
+        }
+        return getTargetIndex(nums, target, mi + 1, end);
+    }
+
+}
