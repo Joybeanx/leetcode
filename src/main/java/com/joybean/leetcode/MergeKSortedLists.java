8badbe6b268da325b1a1e49126fd6a4201d3c17b
joybeanx <ijoybeansang@gmail.com>
2021-03-29 09:58:55 +0800

    update

---
+++ src/main/java/com/joybean/leetcode/MergeKSortedLists.java
@@ -0,0 +1,30 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/merge-k-sorted-lists/">Merge k Sorted Lists</a>
+ *
+ * @author Joybean
+ */
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
