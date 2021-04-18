4ce897544d2b103c1f21c39acf0f3364c322ddff
joybean <jianbing.sjb@alibaba-inc.com>
2021-04-18 16:15:23 +0800

    Solutions for House Robber 1-2

---
+++ src/main/java/com/joybean/leetcode/HouseRobber.java
@@ -0,0 +1,41 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/house-robber/">House Robber</a>
+ *
+ * @author Joybean
+ */
+public class HouseRobber {
+    /**
+     * DP
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob1(int[] nums) {
+        int[] dp = new int[nums.length + 2];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+        return dp[0];
+    }
+
+    /**
+     * DP with state compression
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob2(int[] nums) {
+        int cur, prev1, prev2;
+        cur = prev1 = prev2 = 0;
+        for (
+            int i = nums.length - 1;
+            i >= 0; i--) {
+            cur = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return cur;
+    }
+}
---
+++ src/main/java/com/joybean/leetcode/HouseRobber2.java
@@ -0,0 +1,33 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/house-robber-ii/">House Robber II</a>
+ *
+ * @author Joybean
+ */
+public class HouseRobber2 {
+    /**
+     * DP
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob1(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        return Math.max(robByRange(nums, 0, n - 2), robByRange(nums, 1, n - 1));
+    }
+
+    private static int robByRange(int[] nums, int start, int end) {
+        int cur, prev1, prev2;
+        cur = prev1 = prev2 = 0;
+        for (int i = end; i >= start; i--) {
+            cur = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return cur;
+    }
+}
