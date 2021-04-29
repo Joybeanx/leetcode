a1685d706164771309a8470d037cc0628c6ece00
joybean <joybeanq@gmail.com>
2021-04-29 23:56:30 +0800

    Patching Array

---
+++ src/main/java/com/joybean/leetcode/PatchingArray.java
@@ -0,0 +1,24 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/patching-array/">Patching Array</a>
+ *
+ * @author Joybean
+ */
+public class PatchingArray {
+    public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        long x = 1;
+        int length = nums.length, index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
+            } else {
+                x *= 2;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
