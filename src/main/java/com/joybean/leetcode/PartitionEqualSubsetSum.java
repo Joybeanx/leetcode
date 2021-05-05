9d7c8e02669a15d910d760482ffd09dc9ff9bd38
joybean <joybeanq@gmail.com>
2021-05-05 18:44:31 +0800

    DP with state compression solution for Partition Equal Subset Sum

--- src/main/java/com/joybean/leetcode/PartitionEqualSubsetSum.java
+++ src/main/java/com/joybean/leetcode/PartitionEqualSubsetSum.java
@@ -8,6 +8,11 @@ import java.util.Arrays;
  * @author Joybean
  */
 public class PartitionEqualSubsetSum {
+    /**
+     * DP
+     * @param nums
+     * @return
+     */
     public static boolean canPartition1(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         if (sum % 2 != 0) {
@@ -32,6 +37,29 @@ public class PartitionEqualSubsetSum {
             }
         }
         return dp[nums.length][target];
+    }
+
+    /**
+     * DP with state compression
+     * @param nums
+     * @return
+     */
+    public static boolean canPartition2(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = target; j >= 1; j--) {
+                if (j >= nums[i - 1]) {
+                    dp[j] = dp[j - nums[i - 1]] || dp[j];
+                }
+            }
+        }
+        return dp[target];
     }
 
 }
