0ee887741edff6f5681bfdd4e9b44a5f065aeee7
joybean <joybeanq@gmail.com>
2021-06-13 15:14:44 +0800

    Binary Tree Maximum Path Sum

---
+++ src/main/java/com/joybean/leetcode/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,49 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-maximum-path-sum/">Binary Tree Maximum Path Sum</a>
+ *
+ * @author Joybean
+ */
+public class BinaryTreeMaximumPathSum {
+    //Initialize with minimum value rather than zero
+    int ans = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        singleSideMaxPathSum(root);
+        return ans;
+    }
+
+    /**
+     * Calculate max single side path sum start from root
+     * @param root
+     * @return
+     */
+    private int singleSideMaxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //We don't use negative value
+        int left = Math.max(0, singleSideMaxPathSum(root.left));
+        int right = Math.max(0, singleSideMaxPathSum(root.right));
+        //post-order traversal
+        ans = Math.max(ans, left + right + root.val);
+        return Math.max(left + root.val, right + root.val);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
