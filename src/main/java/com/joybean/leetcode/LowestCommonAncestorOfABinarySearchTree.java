91ed4a03c97054911df73627f96fa0a6dbb6227e
joybean <joybeanq@gmail.com>
2021-07-01 00:04:07 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/com/joybean/leetcode/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,22 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/">Lowest Common Ancestor of
+ * a Binary Search Tree</a>
+ *
+ * @author Joybean
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    //TODO
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) { this.val = val; }
+    }
+}
