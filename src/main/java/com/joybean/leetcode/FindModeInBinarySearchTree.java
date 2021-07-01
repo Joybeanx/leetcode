d5114da9d03abec95f6975427c81c2d24c0536cf
joybean <joybeanq@gmail.com>
2021-07-01 11:47:18 +0800

    Find Mode in Binary Search Tree

--- src/main/java/com/joybean/leetcode/FindModeInBinarySearchTree.java
+++ src/main/java/com/joybean/leetcode/FindModeInBinarySearchTree.java
@@ -1,17 +1,53 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.com/problems/find-mode-in-binary-search-tree/">Find Mode in Binary Search Tree</a>
  *
  * @author Joybean
  */
 public class FindModeInBinarySearchTree {
-    //TODO
-    public int[] findMode(TreeNode root) {
-        return null;
+    private static List<Integer> ans = new ArrayList<>();
+    private static int prev = Integer.MIN_VALUE;
+    private static int count;
+    private static int maxCount;
+
+    public static int[] findMode1(TreeNode root) {
+        inorderTraversal(root);
+        //Because tail nodes doesn't have a chance to update answer so we should do it again after traversal
+        //For example: [1,null,2,2]
+        updAns();
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    public class TreeNode {
+    private static void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        if (prev == root.val) {
+            count++;
+        } else {
+            updAns();
+            count = 1;
+        }
+        prev = root.val;
+        inorderTraversal(root.right);
+    }
+
+    private static void updAns() {
+        if (maxCount == count) {
+            ans.add(prev);
+        } else if (count > maxCount) {
+            maxCount = count;
+            ans.clear();
+            ans.add(prev);
+        }
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -25,5 +61,9 @@ public class FindModeInBinarySearchTree {
             this.left = left;
             this.right = right;
         }
+    }
+
+    public static void main(String[] args) {
+        findMode1(new TreeNode(1, null, new TreeNode(2, new TreeNode(2), null)));
     }
 }
