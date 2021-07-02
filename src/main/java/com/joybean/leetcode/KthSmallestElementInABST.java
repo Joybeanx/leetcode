dc0f6731987ff797ca1d15a61a0488458fe85e47
joybean <joybeanq@gmail.com>
2021-07-02 15:35:15 +0800

    Iterative solution for Kth Smallest Element in a BST

--- src/main/java/com/joybean/leetcode/KthSmallestElementInABST.java
+++ src/main/java/com/joybean/leetcode/KthSmallestElementInABST.java
@@ -1,17 +1,71 @@
 package com.joybean.leetcode;
 
+import java.util.Stack;
+
 /**
  * <a href="https://leetcode.com/problems/kth-smallest-element-in-a-bst/">Kth Smallest Element in a BST</a>
  *
  * @author Joybean
  */
 public class KthSmallestElementInABST {
-    //TODO
-    public int kthSmallest(TreeNode root, int k) {
-        return 0;
+    private static int traversed;
+    private static int ans;
+
+    public static int kthSmallest1(TreeNode root, int k) {
+        inorderTraversal(root, k);
+        return ans;
     }
 
-    public class TreeNode {
+    /**
+     * Recursive solution
+     *
+     * @param root
+     * @param k
+     */
+    private static void inorderTraversal(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, k);
+        if (++traversed == k) {
+            ans = root.val;
+            return;
+        }
+        inorderTraversal(root.right, k);
+    }
+
+    /**
+     * Iterative solution
+     *
+     * @param root
+     * @param k
+     */
+    private static int kthSmallest2(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        int count = 0;
+        while (true) {
+            pushToStack(root, stack);
+            if (!stack.isEmpty()) {
+                TreeNode current = stack.pop();
+                if (++count == k) {
+                    return current.val;
+                }
+                root = current.right;
+            } else {
+                break;
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    private static void pushToStack(TreeNode root, Stack<TreeNode> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
