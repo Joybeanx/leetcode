febeb59988a924a42fa3dca42b94cfed85a3cf78
joybean <joybeanq@gmail.com>
2021-07-06 13:29:19 +0800

    Best Time to Buy and Sell Stock

--- src/main/java/com/joybean/leetcode/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/joybean/leetcode/BestTimeToBuyAndSellStock.java
@@ -6,7 +6,72 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class BestTimeToBuyAndSellStock {
-    public int maxProfit(int[] prices) {
-        return 0;
+    /**
+     * DP using two dimension array
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit1(int[] prices) {
+        int dp[][] = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return Math.max(dp[prices.length - 1][0], 0);
+    }
+
+    /**
+     * DP using one dimension array
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit2(int[] prices) {
+        int dp[] = new int[prices.length];
+        dp[0] = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return dp[prices.length - 1];
+    }
+
+    /**
+     * DP with state compression
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit3(int[] prices) {
+        int ans = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            ans = Math.max(ans, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return ans;
+    }
+
+    /**
+     * Better performance
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit4(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > ans) {
+                ans = prices[i] - minprice;
+            }
+        }
+        return ans;
     }
 }
