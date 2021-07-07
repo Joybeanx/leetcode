b740956dd26444dbd5df577dab124b39e4993dee
joybean <joybeanq@gmail.com>
2021-07-07 23:32:36 +0800

    Largest Rectangle in Histogram

---
+++ src/main/java/com/joybean/leetcode/LargestRectangleInHistogram.java
@@ -0,0 +1,12 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/largest-rectangle-in-histogram/">Largest Rectangle in Histogram</a>
+ *
+ * @author Joybean
+ */
+public class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+}
