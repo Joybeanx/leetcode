65ce7a999bfef36aa88507fb0e46d2f2ad9ed64f
joybean <joybeanq@gmail.com>
2021-07-10 12:17:31 +0800

    Counting Bits

--- src/main/java/com/joybean/leetcode/CountingBits.java
+++ src/main/java/com/joybean/leetcode/CountingBits.java
@@ -6,7 +6,41 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class CountingBits {
-    public int[] countBits(int n) {
-        return null;
+    /**
+     * DP
+     *
+     * @param n
+     * @return
+     */
+    public static int[] countBits1(int n) {
+        if (n == 0) {
+            return new int[1];
+        }
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            int half = i >> 1;
+            //for even numbers, number of ones will be always the same as their halves
+            if ((i & 1) == 0) {
+                dp[i] = dp[half];
+            //for old numbers, number of ones will be number of ones in their halves + 1
+            } else {
+                dp[i] = dp[half] + 1;
+            }
+        }
+        return dp;
+    }
+
+    /**
+     * More concise DP solution
+     *
+     * @param n
+     * @return
+     */
+    public static int[] countBits2(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+        return dp;
     }
 }
