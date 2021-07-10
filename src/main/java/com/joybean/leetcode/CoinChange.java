2b3feea1013f377498920f123208c866b3435a04
joybean <joybeanq@gmail.com>
2021-07-10 17:26:28 +0800

    Coin Change

--- src/main/java/com/joybean/leetcode/CoinChange.java
+++ src/main/java/com/joybean/leetcode/CoinChange.java
@@ -1,12 +1,94 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/coin-change/">Coin Change</a>
  *
  * @author Joybean
  */
 public class CoinChange {
-    public int coinChange(int[] coins, int amount) {
+    /**
+     * DP iterative solution
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public static int coinChange1(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i < coin || dp[i - coin] < 0) {
+                    continue;
+                }
+                if (dp[i] == -1) {
+                    dp[i] = dp[i - coin] + 1;
+                } else {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount];
+    }
+
+    /**
+     * Better DP iterative solution
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public static int coinChange2(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        //initialize with amount+1
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i < coin) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+
+    /**
+     * Best DP iterative solution
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public static int coinChange3(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        // initialize to maximum value
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        //put coins at outer loop to avoid some dp slots
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    /**
+     * DP recursive solution
+     * TODO
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public static int coinChange4(int[] coins, int amount) {
         return 0;
     }
 }
