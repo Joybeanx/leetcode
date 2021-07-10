67868d0b54c8ae21f861c7de201ae3ecc4142ddd
joybean <joybeanq@gmail.com>
2021-07-10 10:43:25 +0800

    Decode Ways

---
+++ src/main/java/com/joybean/leetcode/DecodeWays.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/decode-ways/">Decode Ways</a>
+ *
+ * @author Joybean
+ */
+public class DecodeWays {
+    //TODO
+    public static int numDecodings1(String s) {
+        return 0;
+    }
+}
