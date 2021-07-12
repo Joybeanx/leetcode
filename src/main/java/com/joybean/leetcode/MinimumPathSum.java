d12221eda352b171d2461831b4e1582ee9c66d50
joybean <joybeanq@gmail.com>
2021-07-12 12:59:49 +0800

    Minimum Path Sum

--- src/main/java/com/joybean/leetcode/MinimumPathSum.java
+++ src/main/java/com/joybean/leetcode/MinimumPathSum.java
@@ -6,7 +6,37 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class MinimumPathSum {
-    public int minPathSum(int[][] grid) {
+    /**
+     * DP iterative(bottom-up) solution
+     *
+     * @param grid
+     * @return
+     */
+    public static int minPathSum1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = grid[i][j];
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                } else if (i - 1 >= 0 || j - 1 >= 0) {
+                    dp[i][j] += i - 1 < 0 ? dp[i][j - 1] : dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * DP recursive(top-down) solution
+     * TODO
+     *
+     * @param grid
+     * @return
+     */
+    public static int minPathSum2(int[][] grid) {
         return 0;
     }
 }
