4fd380e8a610bd0f5f0cb96371e16522b8766b15
joybean <joybeanq@gmail.com>
2021-07-14 19:51:52 +0800

    Better solution for Maximum Subarray

--- src/main/java/com/joybean/leetcode/MaximumSubarray.java
+++ src/main/java/com/joybean/leetcode/MaximumSubarray.java
@@ -24,4 +24,37 @@ public class MaximumSubarray {
         }
         return res;
     }
+
+    /**
+     * more cleaner iterative(bottom-up) DP
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray2(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        int ans = nums[0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i - 1], nums[i - 1]);
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
+    }
+
+    /**
+     * iterative(bottom-up) DP with state compression
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray3(int[] nums) {
+        int ans = nums[0];
+        int prev = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.max(prev + nums[i], nums[i]);
+            ans = Math.max(cur, ans);
+            prev = cur;
+        }
+        return ans;
+    }
 }
