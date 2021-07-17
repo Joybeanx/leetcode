c30bd2d1aa83f02247822bda2c82e45bf075d6de
joybean <joybeanq@gmail.com>
2021-07-17 15:21:05 +0800

    Trapping Rain Water

--- src/main/java/com/joybean/leetcode/TrappingRainWater.java
+++ src/main/java/com/joybean/leetcode/TrappingRainWater.java
@@ -6,7 +6,44 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class TrappingRainWater {
-    public int trap(int[] height) {
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param height
+     * @return
+     */
+    public static int trap1(int[] height) {
+        if (height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        //leftMaxHeights[i] represents the max height before index i
+        int[] leftMaxHeights = new int[n];
+        leftMaxHeights[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMaxHeights[i] = Math.max(leftMaxHeights[i - 1], height[i]);
+        }
+        //rightMaxHeights[i] represents the max height after index i
+        int[] rightMaxHeights = new int[n];
+        rightMaxHeights[n - 1] = height[n - 1];
+        for (int j = n - 2; j >= 0; j--) {
+            rightMaxHeights[j] = Math.max(rightMaxHeights[j + 1], height[j]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMaxHeights[i], rightMaxHeights[i]) - height[i];
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/trapping-rain-water/solution/">Using two pointers</a>
+     * TODO
+     *
+     * @param height
+     * @return
+     */
+    public static int trap2(int[] height) {
         return 0;
     }
 }
