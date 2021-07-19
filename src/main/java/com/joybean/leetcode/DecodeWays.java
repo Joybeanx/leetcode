bfb94ca4a62861f5d71b25cc8c9a972b5d8e46f4
joybean <joybeanq@gmail.com>
2021-07-19 18:14:43 +0800

    Decode Ways

--- src/main/java/com/joybean/leetcode/DecodeWays.java
+++ src/main/java/com/joybean/leetcode/DecodeWays.java
@@ -6,8 +6,29 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class DecodeWays {
-    //TODO
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param s
+     * @return
+     */
     public static int numDecodings1(String s) {
-        return 0;
+        //dp[i] represents decoding ways of s[0,i-1]
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            char cur1 = s.charAt(i - 1);
+            if (cur1 >= '1' && cur1 <= '9') {
+                dp[i] = dp[i - 1];
+            }
+            if (i < 2) {
+                continue;
+            }
+            int cur2 = Integer.valueOf(s.substring(i - 2, i));
+            if (cur2 >= 10 && cur2 <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
     }
 }
