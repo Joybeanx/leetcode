49b79a190f6f0f03b387a010c92a2e75a3a00830
joybean <joybeanq@gmail.com>
2021-07-20 11:22:48 +0800

    DP solution for Perfect Squares

--- src/main/java/com/joybean/leetcode/PerfectSquares.java
+++ src/main/java/com/joybean/leetcode/PerfectSquares.java
@@ -6,7 +6,31 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class PerfectSquares {
-    public int numSquares(int n) {
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param n
+     * @return
+     */
+    public static int numSquares1(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = n;
+            for (int l = 1; l * l <= i; l++) {
+                dp[i] = Math.min(dp[i], dp[i - l * l] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * BFS
+     * TODO
+     *
+     * @param n
+     * @return
+     */
+    public static int numSquares2(int n) {
         return 0;
     }
 }
