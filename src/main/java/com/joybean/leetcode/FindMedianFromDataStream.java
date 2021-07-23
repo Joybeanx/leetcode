6ae0979c3a8fee8a19c432dc3d56e474b7ae1802
joybean <joybeanq@gmail.com>
2021-07-23 09:39:42 +0800

    Find Median from Data Stream

---
+++ src/main/java/com/joybean/leetcode/FindMedianFromDataStream.java
@@ -0,0 +1,24 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/find-median-from-data-stream/">Find Median from Data Stream</a>
+ *
+ * @author Joybean
+ */
+public class FindMedianFromDataStream {
+    /**
+     * initialize your data structure here.
+     */
+    //TODO
+    public FindMedianFromDataStream() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+        return 0;
+    }
+}
