d91a2c296732a0087ecfb9193db70b188d86ecca
joybean <joybeanq@gmail.com>
2021-07-24 23:44:24 +0800

    Course Schedule III

---
+++ src/main/java/com/joybean/leetcode/CourseSchedule3.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/course-schedule-iii/">Course Schedule III</a>
+ *
+ * @author Joybean
+ */
+public class CourseSchedule3 {
+    //TODO
+    public static int scheduleCourse1(int[][] courses) {
+        return 0;
+    }
+}
