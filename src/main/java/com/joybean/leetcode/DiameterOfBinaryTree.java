bb71e4de1fc19c68760b001f4a5b60e5692bfadf
joybean <joybeanq@gmail.com>
2021-07-25 21:09:07 +0800

    Diameter of Binary Tree

--- src/main/java/com/joybean/leetcode/DiameterOfBinaryTree.java
+++ src/main/java/com/joybean/leetcode/DiameterOfBinaryTree.java
@@ -6,9 +6,36 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class DiameterOfBinaryTree {
-    //TODO
-    public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+    private static int ans;
+
+    public static int diameterOfBinaryTree1(TreeNode root) {
+        longestPathNodes(root);
+        return ans;
+    }
+
+    private static int longestPathNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftLongestPathNodes = 1 + longestPathNodes(root.left);
+        int rightLongestPathNodes = 1 + longestPathNodes(root.right);
+        ans = Math.max(ans, leftLongestPathNodes + rightLongestPathNodes - 2);
+        return Math.max(leftLongestPathNodes, rightLongestPathNodes);
+    }
+
+    public static int diameterOfBinaryTree2(TreeNode root) {
+        maxDepth(root);
+        return ans;
+    }
+
+    private static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        ans = Math.max(ans, leftMaxDepth + rightMaxDepth);
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
     }
 
     public static class TreeNode {
