47bead08be5cc3a8aefc1a24e23cf20a18761d8d
joybean <joybeanq@gmail.com>
2021-07-26 18:23:37 +0800

    Flatten Binary Tree to Linked List

--- src/main/java/com/joybean/leetcode/FlattenBinaryTreeToLinkedList.java
+++ src/main/java/com/joybean/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -6,8 +6,40 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FlattenBinaryTreeToLinkedList {
-    //TODO
-    public void flatten(TreeNode root) {
+    private static TreeNode prev = null;
+
+    public static void flatten1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        flatten1(left);
+        flatten1(right);
+        root.right = left;
+        TreeNode flattenedTail = root;
+        while (flattenedTail.right != null) {
+            flattenedTail = flattenedTail.right;
+        }
+        flattenedTail.right = right;
+        root.left = null;
+    }
+
+    /**
+     * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/36977/My-short-post-order-traversal
+     * -Java-solution-for-share
+     *
+     * @param root
+     */
+    public static void flatten2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten2(root.right);
+        flatten2(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
 
     public static class TreeNode {
