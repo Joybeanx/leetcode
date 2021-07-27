66ffb8f74f3f9f41c3da99ceba3b1cc227ccf6c8
joybean <joybeanq@gmail.com>
2021-07-27 09:14:44 +0800

    Lazy iterator approach for Flatten Nested List Iterator

--- src/main/java/com/joybean/leetcode/FlattenNestedListIterator2.java
+++ src/main/java/com/joybean/leetcode/FlattenNestedListIterator2.java
@@ -2,6 +2,7 @@ package com.joybean.leetcode;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <a href="https://leetcode.com/problems/flatten-nested-list-iterator/">Flatten Nested List Iterator</a>
@@ -9,24 +10,40 @@ import java.util.List;
  * @author Joybean
  */
 public class FlattenNestedListIterator2 implements Iterator<Integer> {
+    private Stack<NestedInteger> stack;
+
     /**
-     * Lazy iterator
-     * TODO
+     * <a href="https://leetcode.com/problems/flatten-nested-list-iterator/discuss/142999/Load-data-on-next()
+     * -instead-of-hasNext()-JAVA-AC-Solution">Lazy iterator</a>
      *
      * @param nestedList
      */
     public FlattenNestedListIterator2(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        enstack(nestedList);
+    }
 
+    private void enstack(List<NestedInteger> list) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            stack.push(list.get(i));
+        }
+        if (!stack.isEmpty() && !stack.peek().isInteger()) {
+            enstack(stack.pop().getList());
+        }
     }
 
     @Override
     public Integer next() {
-        return 0;
+        Integer res = stack.pop().getInteger();
+        if (!stack.isEmpty() && !stack.peek().isInteger()) {
+            enstack(stack.pop().getList());
+        }
+        return res;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return !stack.isEmpty();
     }
 
     public interface NestedInteger {
