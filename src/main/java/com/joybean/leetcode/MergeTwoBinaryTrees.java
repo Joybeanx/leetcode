9e37dcc9430f1c12b698c8df9512c5fc142822e9
joybean <joybeanq@gmail.com>
2021-07-29 15:45:04 +0800

    Merge Two Binary Trees

--- src/main/java/com/joybean/leetcode/MergeTwoBinaryTrees.java
+++ src/main/java/com/joybean/leetcode/MergeTwoBinaryTrees.java
@@ -1,13 +1,72 @@
 package com.joybean.leetcode;
 
 /**
- * <a href="https://leetcode.com/problems/merge-two-binary-trees/">Diameter of Binary Tree</a>
+ * <a href="https://leetcode.com/problems/merge-two-binary-trees/">Merge Two Binary Trees</a>
  *
  * @author Joybean
  */
 public class MergeTwoBinaryTrees {
-    //TODO
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+    /**
+     * DFS using new TreeNode
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public static TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        }
+        int val = 0;
+        TreeNode left1 = null;
+        TreeNode left2 = null;
+        TreeNode right1 = null;
+        TreeNode right2 = null;
+        if (root1 != null) {
+            val += root1.val;
+            left1 = root1.left;
+            right1 = root1.right;
+        }
+        if (root2 != null) {
+            val += root2.val;
+            left2 = root2.left;
+            right2 = root2.right;
+        }
+        TreeNode root = new TreeNode(val);
+        root.left = mergeTrees1(left1, left2);
+        root.right = mergeTrees1(right1, right2);
+        return root;
+    }
+
+    /**
+     * DFS using original TreeNode
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public static TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees2(root1.left, root2.left);
+        root1.right = mergeTrees2(root1.right, root2.right);
+        return root1;
+    }
+
+    /**
+     * Iterative solution
+     * TODO
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public static TreeNode mergeTrees3(TreeNode root1, TreeNode root2) {
         return null;
     }
 
