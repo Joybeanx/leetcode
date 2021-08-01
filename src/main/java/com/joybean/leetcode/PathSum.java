8fd2142fcc107acc50a2a7b7f17052465cb8b38b
joybean <joybeanq@gmail.com>
2021-08-01 16:56:05 +0800

    Path Sum

--- src/main/java/com/joybean/leetcode/PathSum.java
+++ src/main/java/com/joybean/leetcode/PathSum.java
@@ -6,9 +6,49 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class PathSum {
-    //TODO
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return false;
+    /**
+     * DFS
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public static boolean hasPathSum1(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        return hasPathSumInternal(root, 0, targetSum);
+    }
+
+    private static boolean hasPathSumInternal(TreeNode root, int curSum, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        curSum += root.val;
+        if (root.left == null && root.right == null) {
+            if (curSum == targetSum) {
+                return true;
+            }
+        }
+        return hasPathSumInternal(root.left, curSum, targetSum) || hasPathSumInternal(root.right, curSum, targetSum);
+    }
+
+    /**
+     * More concise DFS solution
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public static boolean hasPathSum2(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        int nextTargetSum = targetSum - root.val;
+        if (root.left == null && root.right == null) {
+            return nextTargetSum == 0;
+        }
+        return hasPathSum2(root.left, nextTargetSum) || hasPathSum2(root.right, nextTargetSum);
     }
 
     public static class TreeNode {
