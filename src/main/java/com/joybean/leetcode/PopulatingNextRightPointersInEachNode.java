73541c5331766bd6707a980351eb3c6dd005959b
joybean <joybeanq@gmail.com>
2021-08-04 23:44:39 +0800

    Populating Next Right Pointers in Each Node

--- src/main/java/com/joybean/leetcode/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/joybean/leetcode/PopulatingNextRightPointersInEachNode.java
@@ -7,9 +7,55 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class PopulatingNextRightPointersInEachNode {
-    //TODO
-    public Node connect(Node root) {
-        return null;
+    /**
+     * DFS:O(log n) space
+     *
+     * @param root
+     * @return
+     */
+    public static Node connect1(Node root) {
+        int height = getHeight(root);
+        Node[] rightSides = new Node[height];
+        connectInternal(root, 0, rightSides);
+        return root;
+    }
+
+    private static int getHeight(Node root) {
+        int height = 0;
+        while (root != null) {
+            height++;
+            root = root.right;
+        }
+        return height;
+    }
+
+    private static void connectInternal(Node root, int level, Node[] rightSides) {
+        if (root == null) {
+            return;
+        }
+        root.next = rightSides[level];
+        rightSides[level] = root;
+        connectInternal(root.right, level + 1, rightSides);
+        connectInternal(root.left, level + 1, rightSides);
+    }
+
+    /**
+     * DFS:O(1) space
+     *
+     * @param root
+     * @return
+     */
+    public static Node connect2(Node root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        root.left.next = root.right;
+        if (root.next != null) {
+            root.right.next = root.next.left;
+        }
+        connect2(root.left);
+        connect2(root.right);
+        return root;
     }
 
     public static class Node {
