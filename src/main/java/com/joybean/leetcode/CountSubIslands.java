bc9c1dd31800b4718d9c586a686bea94576fb8fc
joybean <joybeanq@gmail.com>
2021-08-07 00:08:16 +0800

    Union Find problems

---
+++ src/main/java/com/joybean/leetcode/CountSubIslands.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/count-sub-islands/">Count Sub Islands</a>
+ *
+ * @author Joybean
+ */
+public class CountSubIslands {
+    //TODO
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/joybean/leetcode/MaxAreaOfIsland.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/max-area-of-island/">Max Area of Island</a>
+ *
+ * @author Joybean
+ */
+public class MaxAreaOfIsland {
+    //TODO
+    public int maxAreaOfIsland(int[][] grid) {
+        return 0;
+    }
+}
