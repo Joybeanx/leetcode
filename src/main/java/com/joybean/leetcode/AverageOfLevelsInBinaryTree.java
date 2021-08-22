10ca072726af13ebaec60c69f974406e35235a06
joybean <joybeanq@gmail.com>
2021-08-23 00:14:47 +0800

    Optimized solution for Average of Levels in Binary Tree

--- src/main/java/com/joybean/leetcode/AverageOfLevelsInBinaryTree.java
+++ src/main/java/com/joybean/leetcode/AverageOfLevelsInBinaryTree.java
@@ -12,12 +12,12 @@ import java.util.Map;
  */
 public class AverageOfLevelsInBinaryTree {
     /**
-     * DFS
+     * DFS using two maps
      *
      * @param root
      * @return
      */
-    public List<Double> averageOfLevels(TreeNode root) {
+    public static List<Double> averageOfLevels1(TreeNode root) {
         Map<Integer, Double> levelSumMap = new HashMap<>();
         Map<Integer, Integer> levelCountMap = new HashMap<>();
         List<Double> ans = new ArrayList<>();
@@ -28,7 +28,7 @@ public class AverageOfLevelsInBinaryTree {
         return ans;
     }
 
-    private void helper(TreeNode root, int level, Map<Integer, Double> levelSumMap,
+    private static void helper(TreeNode root, int level, Map<Integer, Double> levelSumMap,
         Map<Integer, Integer> levelCountMap) {
         if (root == null) {
             return;
@@ -37,6 +37,47 @@ public class AverageOfLevelsInBinaryTree {
         levelCountMap.put(level, levelCountMap.getOrDefault(level, 0) + 1);
         helper(root.left, level + 1, levelSumMap, levelCountMap);
         helper(root.right, level + 1, levelSumMap, levelCountMap);
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/average-of-levels-in-binary-tree/solution/">>DFS using two lists</a>
+     *
+     * @param root
+     * @return
+     */
+    public static List<Double> averageOfLevels2(TreeNode root) {
+        List<Integer> count = new ArrayList<>();
+        List<Double> res = new ArrayList<>();
+        average(root, 0, res, count);
+        for (int i = 0; i < res.size(); i++) {
+            res.set(i, res.get(i) / count.get(i));
+        }
+        return res;
+    }
+
+    public static void average(TreeNode node, int level, List<Double> sum, List<Integer> count) {
+        if (node == null) {
+            return;
+        }
+        if (level < sum.size()) {
+            sum.set(level, sum.get(level) + node.val);
+            count.set(level, count.get(level) + 1);
+        } else {
+            sum.add(1.0 * node.val);
+            count.add(1);
+        }
+        average(node.left, level + 1, sum, count);
+        average(node.right, level + 1, sum, count);
+    }
+
+    /**
+     * BFS
+     *
+     * @param root
+     * @return
+     */
+    public static List<Double> averageOfLevels3(TreeNode root) {
+        return null;
     }
 
     public class TreeNode {
