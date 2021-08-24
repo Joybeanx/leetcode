844812bdd8d8235ac1aff5a3f6a75c9f74aa0f7f
joybean <joybeanq@gmail.com>
2021-08-24 20:27:58 +0800

    N-ary Tree Level Order Traversal

--- src/main/java/com/joybean/leetcode/NaryTreeLevelOrderTraversal.java
+++ src/main/java/com/joybean/leetcode/NaryTreeLevelOrderTraversal.java
@@ -1,6 +1,9 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * <a href="https://leetcode.com/problems/n-ary-tree-level-order-traversal/">N-ary Tree Level Order Traversal</a>
@@ -8,9 +11,81 @@ import java.util.List;
  * @author Joybean
  */
 public class NaryTreeLevelOrderTraversal {
-    //TODO
-    public List<List<Integer>> levelOrder(Node root) {
-        return null;
+    /**
+     * DFS
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder1(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(root, 0, ans);
+        return ans;
+    }
+
+    private static void helper(Node node, int level, List<List<Integer>> ans) {
+        if (node == null) {
+            return;
+        }
+        if (ans.size() == level) {
+            ans.add(new ArrayList<>());
+        }
+        ans.get(level).add(node.val);
+        if (node.children == null) {
+            return;
+        }
+        for (Node child : node.children) {
+            helper(child, level + 1, ans);
+        }
+    }
+
+    /**
+     * BFS using List:Time Limit Exceeded
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder2(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Node> children = root.children;
+        while (children != null) {
+            List<Integer> levelValues = new ArrayList<>();
+            List<Node> nextChildren = new ArrayList<>();
+            for (Node child : children) {
+                levelValues.add(child.val);
+                if (child.children != null) {
+                    nextChildren.addAll(child.children);
+                }
+            }
+            ans.add(levelValues);
+            children = nextChildren;
+        }
+        return ans;
+    }
+
+    /**
+     * BFS using Queue
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder3(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                levelNodes.add(queue.peek().val);
+                queue.addAll(queue.poll().children);
+            }
+            res.add(levelNodes);
+        }
+        return res;
     }
 
     public class Node {
