9b3d4eb81c4b42feb67d95f935ccda040e2abd7c
joybean <joybeanq@gmail.com>
2021-08-24 20:27:34 +0800

    Cousins in Binary Tree

--- src/main/java/com/joybean/leetcode/CousinsInBinaryTree.java
+++ src/main/java/com/joybean/leetcode/CousinsInBinaryTree.java
@@ -6,12 +6,56 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class CousinsInBinaryTree {
-    //TODO
-    public boolean isCousins(TreeNode root, int x, int y) {
+    private static TreeNode xParent;
+    private static TreeNode yParent;
+    private static int xLevel;
+    private static int yLevel;
+
+    /**
+     * DFS
+     *
+     * @param root
+     * @param x
+     * @param y
+     * @return
+     */
+    public static boolean isCousins1(TreeNode root, int x, int y) {
+        helper(null, root, 0, x, y);
+        if (xParent != null && yParent != null) {
+            return xParent != yParent && xLevel == yLevel;
+        }
         return false;
     }
 
-    public class TreeNode {
+    public static void helper(TreeNode parent, TreeNode node, int level, int x, int y) {
+        if (node == null) {
+            return;
+        }
+        if (x == node.val) {
+            xParent = parent;
+            xLevel = level;
+        } else if (y == node.val) {
+            yParent = parent;
+            yLevel = level;
+        }
+        helper(node, node.left, level + 1, x, y);
+        helper(node, node.right, level + 1, x, y);
+    }
+
+    /**
+     * BFS
+     * TODO
+     *
+     * @param root
+     * @param x
+     * @param y
+     * @return
+     */
+    public static boolean isCousins2(TreeNode root, int x, int y) {
+        return false;
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
