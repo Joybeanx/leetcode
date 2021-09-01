35bdc2034e66a0e57a142142349581e71ee7897b
joybean <joybeanq@gmail.com>
2021-09-01 22:01:20 +0800

    Find Median from Data Stream

--- src/main/java/com/joybean/leetcode/FindMedianFromDataStream.java
+++ src/main/java/com/joybean/leetcode/FindMedianFromDataStream.java
@@ -1,24 +1,43 @@
 package com.joybean.leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * <a href="https://leetcode.com/problems/find-median-from-data-stream/">Find Median from Data Stream</a>
  *
  * @author Joybean
  */
 public class FindMedianFromDataStream {
-    /**
-     * initialize your data structure here.
-     */
-    //TODO
-    public FindMedianFromDataStream() {
+    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> maxHeap;
+    private boolean odd;
 
+    /**
+     * <a href="https://leetcode.com/problems/find-median-from-data-stream/discuss/74047/JavaPython-two-heap-solution-O(log-n)
+     * -add-O(1)-find">Two heaps solution</a>
+     */
+    public FindMedianFromDataStream() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
     }
 
     public void addNum(int num) {
-
+        if (odd) {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        } else {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        }
+        odd = !odd;
     }
 
     public double findMedian() {
-        return 0;
+        if (odd) {
+            return minHeap.peek();
+        } else {
+            return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        }
     }
 }
