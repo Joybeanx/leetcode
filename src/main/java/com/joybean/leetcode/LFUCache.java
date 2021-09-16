555480afc33ea7ebb399a7260ff982b70b45f58b
joybean <joybeanq@gmail.com>
2021-09-16 09:19:15 +0800

    LFU problem

---
+++ src/main/java/com/joybean/leetcode/LFUCache.java
@@ -0,0 +1,21 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/lfu-cache/">LFU Cache</a>
+ *
+ * @author Joybean
+ */
+public class LFUCache {
+    //TODO
+    public LFUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
