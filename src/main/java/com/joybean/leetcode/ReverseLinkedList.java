b98b67991eb2c71d74ca012e86e367c94270447b
joybean <joybeanq@gmail.com>
2021-09-28 23:20:25 +0800

    Reverse Linked List

--- src/main/java/com/joybean/leetcode/ReverseLinkedList.java
+++ src/main/java/com/joybean/leetcode/ReverseLinkedList.java
@@ -6,9 +6,46 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class ReverseLinkedList {
-    //TODO
-    public ListNode reverseList(ListNode head) {
-        return null;
+    /**
+     * Iterative solution
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList1(ListNode head) {
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = next;
+            next = cur;
+            cur = tmp;
+        }
+        return next;
+    }
+
+    /**
+     * Recursive solution
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode ans = helper(head, head.next);
+        head.next = null;
+        return ans;
+    }
+
+    private static ListNode helper(ListNode prev, ListNode cur) {
+        if (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            return helper(cur, next);
+        }
+        return prev;
     }
 
     public class ListNode {
