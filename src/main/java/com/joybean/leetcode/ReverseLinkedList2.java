266578c21788399298cdad861ffc083a03645ec9
joybean <joybeanq@gmail.com>
2021-10-28 17:31:33 +0800

    Solution for Reverse Linked List II

--- src/main/java/com/joybean/leetcode/ReverseLinkedList2.java
+++ src/main/java/com/joybean/leetcode/ReverseLinkedList2.java
@@ -6,12 +6,34 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class ReverseLinkedList2 {
-    //TODO
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        return null;
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        int index = 1;
+        ListNode reversedTail = head;
+        ListNode prefix = null;
+        while (index < left) {
+            prefix = reversedTail;
+            reversedTail = reversedTail.next;
+            index++;
+        }
+        ListNode cur = reversedTail;
+        ListNode reversedHead = null;
+        ListNode suffix = reversedTail.next;
+        while (index <= right) {
+            suffix = cur.next;
+            cur.next = reversedHead;
+            reversedHead = cur;
+            cur = suffix;
+            index++;
+        }
+        reversedTail.next = suffix;
+        if (prefix == null) {
+            return reversedHead;
+        }
+        prefix.next = reversedHead;
+        return head;
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
