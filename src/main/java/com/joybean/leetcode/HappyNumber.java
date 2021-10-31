7ee81a6de418cdd8ea7c2ab1cf63b17cd7d6a312
joybean <joybeanq@gmail.com>
2021-10-31 23:39:47 +0800

    Happy Number

---
+++ src/main/java/com/joybean/leetcode/HappyNumber.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/happy-number/">Happy Number</a>
+ *
+ * @author Joybean
+ */
+public class HappyNumber {
+    //TODO
+    public boolean isHappy(int n) {
+        return false;
+    }
+}
