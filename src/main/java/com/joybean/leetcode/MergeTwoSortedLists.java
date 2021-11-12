c5644b384c7665edfd31f2a882d468c65a5a79dc
joybean <joybeanq@gmail.com>
2021-11-12 15:13:51 +0800

    Solutions for Merge Two Sorted Lists

--- src/main/java/com/joybean/leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/joybean/leetcode/MergeTwoSortedLists.java
@@ -6,9 +6,53 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class MergeTwoSortedLists {
-    //TODO
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+    /**
+     * Iterative solution using two pointers
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode start = new ListNode(0);
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode cur = start;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                cur.next = p1;
+                p1 = p1.next;
+            } else {
+                cur.next = p2;
+                p2 = p2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = p1 != null ? p1 : p2;
+        return start.next;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/merge-two-sorted-lists/discuss/9715/Java-1-ms-4-lines-codes-using
+     * -recursion">Recursive solution</a>
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists2(l1, l2.next);
+        return l2;
     }
 
     public static class ListNode {
