431252cbc015aef0dea9bb483f6f1111807a266e
joybean <joybeanq@gmail.com>
2021-11-13 20:32:20 +0800

    Solution for Remove Duplicates from Sorted List

--- src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedList.java
@@ -6,9 +6,66 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RemoveDuplicatesFromSortedList {
-    //TODO
-    public ListNode deleteDuplicates(ListNode head) {
-        return null;
+    /**
+     * Iterative solution
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates1(ListNode head) {
+        ListNode cur = head;
+        ListNode lastUnique = cur;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            if (cur.val != lastUnique.val) {
+                lastUnique.next = cur;
+                lastUnique = cur;
+            } else {
+                lastUnique.next = null;
+            }
+            cur = tmp;
+        }
+        return head;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-list/discuss/28730/Concise-solution-and
+     * -memory-freeing">Clean iterative solution </a>
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                //Skip duplicate
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * Recursive solution
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates3(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates3(head.next);
+        if (head.val == head.next.val) {
+            return head.next;
+        }
+        return head;
     }
 
     public static class ListNode {
