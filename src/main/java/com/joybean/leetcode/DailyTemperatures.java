8a5afcda456879ebfca61dc5500e929c97f277a0
joybean <joybeanq@gmail.com>
2021-11-15 22:30:51 +0800

    Monotonically stack solution for Daily Temperatures

--- src/main/java/com/joybean/leetcode/DailyTemperatures.java
+++ src/main/java/com/joybean/leetcode/DailyTemperatures.java
@@ -1,13 +1,30 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/daily-temperatures/">Daily Temperatures</a>
  *
  * @author Joybean
  */
 public class DailyTemperatures {
-    //TODO
-    public int[] dailyTemperatures(int[] temperatures) {
-        return null;
+    /**
+     * Monotonically increasing stack
+     *
+     * @param temperatures
+     * @return
+     */
+    public static int[] dailyTemperatures1(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        for (int i = 1; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                ans[stack.peek()] = i - stack.poll();
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 }
