e5272fea13123e3ea6a23691a127061f2d554b73
joybean <joybeanq@gmail.com>
2021-11-20 11:19:48 +0800

    Add todo

--- src/main/java/com/joybean/leetcode/PalindromeLinkedList.java
+++ src/main/java/com/joybean/leetcode/PalindromeLinkedList.java
@@ -6,13 +6,21 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class PalindromeLinkedList {
+    /**
+     * Iterative solution:reversed left half == right half?
+     *
+     * @param head
+     * @return
+     */
     public static boolean isPalindrome1(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
+        //Slow will reach the middle and fast will reach the end
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
+        //Reverse nodes from start to middle
         ListNode leftHead = reverseLeftHalf(head, slow);
         //When linked list has odd nodes,let right half smaller so that the two halves have same length
         //for example [1,3,1], slow pointer will stop at 3
@@ -20,6 +28,7 @@ public class PalindromeLinkedList {
         if (fast != null) {
             rightHead = slow.next;
         }
+        //Compare two halves
         while (rightHead != null && leftHead != null) {
             if (rightHead.val != leftHead.val) {
                 return false;
@@ -39,6 +48,18 @@ public class PalindromeLinkedList {
             from = tmp;
         }
         return newHead;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/palindrome-linked-list/discuss/181453/Java-or-5-lines-method-or-2ms-or
+     * -O(n)-or-recursive-or-easy-to-understand">Recursive solution</a>
+     * TODO
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindrome2(ListNode head) {
+        return false;
     }
 
     public static class ListNode {
