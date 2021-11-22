e380239056638ed46d078c9019e824f912b42a8c
joybean <joybeanq@gmail.com>
2021-11-22 17:44:35 +0800

    Monotonic stack solution for Remove Duplicate Letters

--- src/main/java/com/joybean/leetcode/RemoveDuplicateLetters.java
+++ src/main/java/com/joybean/leetcode/RemoveDuplicateLetters.java
@@ -1,13 +1,55 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/remove-duplicate-letters/">Remove Duplicate Letters</a>
  *
  * @author Joybean
  */
 public class RemoveDuplicateLetters {
-    //TODO
-    public String removeDuplicateLetters(String s) {
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicate-letters/discuss/76769/Java-solution-using-Stack-with
+     * -comments">Monotonic stack</a>
+     *
+     * @param s
+     * @return
+     */
+    public static String removeDuplicateLetters1(String s) {
+        char[] chars = s.toCharArray();
+        int[] remainingFreq = new int[26];
+        for (char ch : chars) {
+            remainingFreq[ch - 'a']++;
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        boolean[] inStack = new boolean[26];
+        for (char ch : chars) {
+            int index = ch - 'a';
+            remainingFreq[index]--;
+            if (inStack[index]) {
+                continue;
+            }
+            while (!stack.isEmpty() && stack.peek() > ch && remainingFreq[stack.peek() - 'a'] > 0) {
+                inStack[stack.pop() - 'a'] = false;
+            }
+            stack.push(ch);
+            inStack[index] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Recursive solution
+     *
+     * @param s
+     * @return
+     */
+    public static String removeDuplicateLetters2(String s) {
         return null;
     }
 }
