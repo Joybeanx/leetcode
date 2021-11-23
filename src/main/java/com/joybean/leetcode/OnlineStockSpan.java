ad4bc57196a5c6345f6e75f1c37f8384d35d88b4
joybean <joybeanq@gmail.com>
2021-11-23 09:58:30 +0800

    Stack solution for Online Stock Span

--- src/main/java/com/joybean/leetcode/OnlineStockSpan.java
+++ src/main/java/com/joybean/leetcode/OnlineStockSpan.java
@@ -1,17 +1,31 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.com/problems/online-stock-span/">Online Stock Span</a>
  *
  * @author Joybean
  */
 public class OnlineStockSpan {
-    //TODO
-    public OnlineStockSpan() {
+    Deque<Integer> stack;
+    List<int[]> list;
 
+    public OnlineStockSpan() {
+        stack = new ArrayDeque<>();
+        list = new ArrayList<>();
     }
 
     public int next(int price) {
-        return 0;
+        int span = 1;
+        while (!stack.isEmpty() && list.get(stack.peek())[0] <= price) {
+            span += list.get(stack.pop())[1];
+        }
+        list.add(new int[] {price, span});
+        stack.push(list.size() - 1);
+        return span;
     }
 }
