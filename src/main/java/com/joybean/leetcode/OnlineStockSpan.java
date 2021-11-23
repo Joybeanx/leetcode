7cf064211396e5f04277c1364c12b7aef55e0927
joybean <joybeanq@gmail.com>
2021-11-23 11:52:35 +0800

    Better stack solution for Online Stock Span

--- src/main/java/com/joybean/leetcode/OnlineStockSpan.java
+++ src/main/java/com/joybean/leetcode/OnlineStockSpan.java
@@ -11,21 +11,47 @@ import java.util.List;
  * @author Joybean
  */
 public class OnlineStockSpan {
-    Deque<Integer> stack;
+    Deque<Integer> stack1;
     List<int[]> list;
 
+    Deque<int[]> stack2;
+
     public OnlineStockSpan() {
-        stack = new ArrayDeque<>();
+        stack1 = new ArrayDeque<>();
         list = new ArrayList<>();
+
+        stack2 = new ArrayDeque<>();
     }
 
-    public int next(int price) {
+    /**
+     * Stack solution
+     *
+     * @param price
+     * @return
+     */
+    public int next1(int price) {
         int span = 1;
-        while (!stack.isEmpty() && list.get(stack.peek())[0] <= price) {
-            span += list.get(stack.pop())[1];
+        while (!stack1.isEmpty() && list.get(stack1.peek())[0] <= price) {
+            span += list.get(stack1.pop())[1];
         }
         list.add(new int[] {price, span});
-        stack.push(list.size() - 1);
+        stack1.push(list.size() - 1);
+        return span;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/online-stock-span/discuss/640358/JAVA-Solution-With-visualization-and-easy
+     * -explained!">Better stack solution</a>
+     *
+     * @param price
+     * @return
+     */
+    public int next2(int price) {
+        int span = 1;
+        while (!stack2.isEmpty() && stack2.peek()[0] <= price) {
+            span += stack2.pop()[1];
+        }
+        stack2.push(new int[] {price, span});
         return span;
     }
 }
