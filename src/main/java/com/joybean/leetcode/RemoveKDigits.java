9e039ffbdbdec764d3db223ccd7630f559212850
joybean <joybeanq@gmail.com>
2021-11-27 00:13:10 +0800

    Monotonic stack solution for Remove K Digits

--- src/main/java/com/joybean/leetcode/RemoveKDigits.java
+++ src/main/java/com/joybean/leetcode/RemoveKDigits.java
@@ -1,14 +1,48 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/remove-k-digits/">Remove K Digits</a>
  *
  * @author Joybean
  */
 public class RemoveKDigits {
-    //TODO
-    public String removeKdigits(String num, int k) {
-        return null;
+    /**
+     * Monotonic stack
+     *
+     * @param num
+     * @param k
+     * @return
+     */
+    public static String removeKdigits1(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : num.toCharArray()) {
+            while (!stack.isEmpty() && stack.peek() > ch && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(ch);
+        }
+        //case like "1111" or "1234"
+        while (k-- > 0) {
+            stack.pop();
+        }
 
+        //trim leading zeros
+        while (!stack.isEmpty() && stack.peekLast() == '0') {
+            stack.pollLast();
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        String ans = sb.toString();
+        if (ans.isEmpty()) {
+            return "0";
+        }
+        return ans;
     }
 }
