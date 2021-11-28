08813340ac77ebf3f5bf6b38ea8b89dff067f0e2
joybean <joybeanq@gmail.com>
2021-11-28 12:55:42 +0800

    Solution for Maximum Width Ramp

--- src/main/java/com/joybean/leetcode/MaximumWidthRamp.java
+++ src/main/java/com/joybean/leetcode/MaximumWidthRamp.java
@@ -6,8 +6,46 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class MaximumWidthRamp {
-    //TODO
-    public int maxWidthRamp(int[] nums) {
+    /**
+     * Time complexity：O(n^2)，TLE
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxWidthRamp1(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[j] >= nums[i]) {
+                    ans = Math.max(ans, j - i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/maximum-width-ramp/discuss/208348/JavaC%2B%2BPython-O(N)-Using-Stack">Binary
+     * search</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxWidthRamp2(int[] nums) {
+        return 0;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/maximum-width-ramp/discuss/208348/JavaC%2B%2BPython-O(N)-Using-Stack">Stack
+     * solution</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxWidthRamp3(int[] nums) {
         return 0;
     }
 }
