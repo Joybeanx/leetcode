4d1fd9d0054fdca86940d5d92759ab0b3c35da88
joybean <joybeanq@gmail.com>
2021-11-28 21:08:39 +0800

    Solution for Smallest Subsequence of Distinct

--- src/main/java/com/joybean/leetcode/SmallestSubsequenceOfDistinctCharacters.java
+++ src/main/java/com/joybean/leetcode/SmallestSubsequenceOfDistinctCharacters.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/">Smallest Subsequence of Distinct
  * Characters</a>
@@ -7,8 +10,36 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class SmallestSubsequenceOfDistinctCharacters {
-    //TODO
+    /**
+     * This question is the same as: Remove Duplicate Letters
+     * @param s
+     * @return
+     * @see com.joybean.leetcode.RemoveDuplicateLetters
+     */
     public String smallestSubsequence(String s) {
-        return null;
+        char[] chars = s.toCharArray();
+        int[] remainingCnt = new int[26];
+        for (char ch : chars) {
+            remainingCnt[ch - 'a']++;
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        boolean[] inStack = new boolean[26];
+        for (char ch : chars) {
+            int index = ch - 'a';
+            remainingCnt[index]--;
+            if (inStack[index]) {
+                continue;
+            }
+            while (!stack.isEmpty() && stack.peek() > ch && remainingCnt[stack.peek() - 'a'] > 0) {
+                inStack[stack.pop() - 'a'] = false;
+            }
+            stack.push(ch);
+            inStack[index] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        return sb.toString();
     }
 }
