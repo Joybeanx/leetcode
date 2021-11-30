c949293c12278bcc7ca69a35106d4afef507c280
joybean <joybeanq@gmail.com>
2021-11-30 16:50:17 +0800

    Stack solution for Maximum Width Ramp

--- src/main/java/com/joybean/leetcode/MaximumWidthRamp.java
+++ src/main/java/com/joybean/leetcode/MaximumWidthRamp.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/maximum-width-ramp/">Maximum Width Ramp</a>
  *
@@ -40,12 +43,36 @@ public class MaximumWidthRamp {
     /**
      * <a href="https://leetcode.com/problems/maximum-width-ramp/discuss/208348/JavaC%2B%2BPython-O(N)-Using-Stack">Stack
      * solution</a>
-     * TODO
      *
      * @param nums
      * @return
      */
     public static int maxWidthRamp3(int[] nums) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+        int ans = 0;
+        //Skip unnecessary calculation when j is less than current max width
+        for (int j = nums.length - 1; j >= ans; j--) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
+                ans = Math.max(ans, j - stack.pop());
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://medium.com/@yashgirdhar/maximum-width-ramp-problem-b1687a40d0bf">Two pointer</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxWidthRamp4(int[] nums) {
         return 0;
     }
 }
