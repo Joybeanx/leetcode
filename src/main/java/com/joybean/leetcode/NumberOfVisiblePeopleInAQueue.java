1639ef49c83639ca65714ed2a79e95bc2518f6a3
joybean <joybeanq@gmail.com>
2021-12-02 23:32:20 +0800

    Stack solution for Number of Visible People in a Queue

--- src/main/java/com/joybean/leetcode/NumberOfVisiblePeopleInAQueue.java
+++ src/main/java/com/joybean/leetcode/NumberOfVisiblePeopleInAQueue.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/number-of-visible-people-in-a-queue/">Number of Visible People in a Queue</a>
  *
@@ -24,6 +27,30 @@ public class NumberOfVisiblePeopleInAQueue {
                 max = Math.max(max, heights[j]);
             }
             ans[i] = cnt;
+        }
+        return ans;
+    }
+
+    /**
+     * Stack solution,TLE
+     *
+     * @param heights
+     * @return
+     */
+    public static int[] canSeePersonsCount2(int[] heights) {
+        int[] ans = new int[heights.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = heights.length - 1; i >= 0; i--) {
+            int cnt = 0;
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
+                stack.pop();
+                cnt++;
+            }
+            if (!stack.isEmpty()) {
+                cnt++;
+            }
+            ans[i] = cnt;
+            stack.push(i);
         }
         return ans;
     }
