21d0b9fa004f85136dc1ad66df6ea6c6ffd75b9c
joybean <joybeanq@gmail.com>
2021-12-04 11:54:44 +0800

    Another monotonic stack solution for Number of Visible People in a Queue

--- src/main/java/com/joybean/leetcode/NumberOfVisiblePeopleInAQueue.java
+++ src/main/java/com/joybean/leetcode/NumberOfVisiblePeopleInAQueue.java
@@ -32,7 +32,7 @@ public class NumberOfVisiblePeopleInAQueue {
     }
 
     /**
-     * Stack solution,TLE
+     * Monotonic stack
      *
      * @param heights
      * @return
@@ -40,18 +40,41 @@ public class NumberOfVisiblePeopleInAQueue {
     public static int[] canSeePersonsCount2(int[] heights) {
         int[] ans = new int[heights.length];
         Deque<Integer> stack = new ArrayDeque<>();
+        //Moving from end to start,use monotonically decreasing stack
         for (int i = heights.length - 1; i >= 0; i--) {
-            int cnt = 0;
             while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
                 stack.pop();
-                cnt++;
+                ans[i]++;
             }
             if (!stack.isEmpty()) {
-                cnt++;
+                ans[i]++;
             }
-            ans[i] = cnt;
             stack.push(i);
         }
         return ans;
     }
+
+    /**
+     * <a href="https://leetcode.com/problems/number-of-visible-people-in-a-queue/discuss/1359707/JavaC%2B%2BPython
+     * -Stack-Solution-Next-Greater-Element">Monotonic stack</a>
+     *
+     * @param heights
+     * @return
+     */
+    public static int[] canSeePersonsCount3(int[] heights) {
+        int[] ans = new int[heights.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        //Moving from start to end,use monotonically decreasing stack
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
+                ans[stack.pop()]++;
+            }
+            if (!stack.isEmpty()) {
+                ans[stack.peek()]++;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
 }
