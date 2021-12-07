d882d9cea8639123853a760c1064c8088f0be43b
joybean <joybeanq@gmail.com>
2021-12-07 20:53:46 +0800

    Monotonic stack solutions for Final Prices With a Special Discount in a Shop

--- src/main/java/com/joybean/leetcode/FinalPricesWithASpecialDiscountInAShop.java
+++ src/main/java/com/joybean/leetcode/FinalPricesWithASpecialDiscountInAShop.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/">Final Prices With a Special
  * Discount in a Shop</a>
@@ -7,8 +10,41 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FinalPricesWithASpecialDiscountInAShop {
-    //TODO
-    public int[] finalPrices(int[] prices) {
-        return null;
+    /**
+     * Monotonic stack
+     *
+     * @param prices
+     * @return
+     */
+    public static int[] finalPrices1(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] ans = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            ans[i] = prices[i];
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int top = stack.pop();
+                ans[top] = prices[top] - prices[i];
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/discuss/685390/JavaC%2B
+     * %2BPython-Stack-One-Pass">Monotonic stack</a>
+     *
+     * @param prices
+     * @return
+     */
+    public static int[] finalPrices2(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                prices[stack.pop()] -= prices[i];
+            }
+            stack.push(i);
+        }
+        return prices;
     }
 }
