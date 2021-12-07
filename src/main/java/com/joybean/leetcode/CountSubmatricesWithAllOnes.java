a70ee8cafdc48b0770ec6a3b6f9b9cd96da353aa
joybean <joybeanq@gmail.com>
2021-12-07 14:06:48 +0800

    Brute force solution for Count submatrices With All Ones

--- src/main/java/com/joybean/leetcode/CountSubmatricesWithAllOnes.java
+++ src/main/java/com/joybean/leetcode/CountSubmatricesWithAllOnes.java
@@ -6,8 +6,64 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class CountSubmatricesWithAllOnes {
-    //TODO
-    public int numSubmat(int[][] mat) {
+    /**
+     * <a href="https://krains.gitee.io/blogs/Algorithm&Data%20Structure/LeetCode/1504
+     * .%20%E7%BB%9F%E8%AE%A1%E5%85%A8%201%20%E5%AD%90%E7%9F%A9%E5%BD%A2.html#%E6%9A%B4%E5%8A%9B%E8%A7%A3">Brute
+     * force</a>
+     *
+     * @param mat
+     * @return
+     */
+    public static int numSubmat1(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int ans = 0;
+
+        // heights of every 1
+        int[][] heights = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    heights[i][j] = i == 0 ? 1 : heights[i - 1][j] + 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) { continue; }
+                int min = heights[i][j];
+                // Moving from right to left,add up current minimum height
+                for (int k = j; k >= 0 && heights[i][k] != 0; k--) {
+                    min = Math.min(min, heights[i][k]);
+                    ans += min;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/720265/Java-Detailed
+     * -Explanation-From-O(MNM)-to-O(MN)-by-using-Stack"">Brute force</a>
+     * TODO
+     *
+     * @param mat
+     * @return
+     */
+    public static int numSubmat2(int[][] mat) {
+        return 0;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/720265/Java-Detailed
+     * -Explanation-From-O(MNM)-to-O(MN)-by-using-Stack"">Monotonic stack</a>
+     * TODO
+     *
+     * @param mat
+     * @return
+     */
+    public static int numSubmat3(int[][] mat) {
         return 0;
     }
 }
