fecb745d88984c383725e006e80ea544f6c733ba
joybean <joybeanq@gmail.com>
2021-12-08 09:22:25 +0800

    Monotonic stack solution for Find the Most Competitive Subsequence

--- src/main/java/com/joybean/leetcode/FindTheMostCompetitiveSubsequence.java
+++ src/main/java/com/joybean/leetcode/FindTheMostCompetitiveSubsequence.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/find-the-most-competitive-subsequence/">Find the Most
  * Competitive Subsequence</a>
@@ -7,8 +10,41 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FindTheMostCompetitiveSubsequence {
-    //TODO
-    public int[] mostCompetitive(int[] nums, int k) {
+    /**
+     * Monotonic stack
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] mostCompetitive1(int[] nums, int k) {
+        int[] ans = new int[k];
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = nums.length - k;
+        for (int num : nums) {
+            while (!stack.isEmpty() && num < stack.peek() && n > 0) {
+                stack.pop();
+                n--;
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < k && !stack.isEmpty(); i++) {
+            ans[i] = stack.pollLast();
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/find-the-most-competitive-subsequence/discuss/952786/JavaC%2B%2BPython
+     * -One-Pass-Stack-Solution">Array solution</a>
+     * TODO
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] mostCompetitive2(int[] nums, int k) {
         return null;
     }
+
 }
