1b1e4e5d446f7c3efccb4c04af56ebc3bfbbf401
joybean <joybeanq@gmail.com>
2021-12-11 13:18:04 +0800

    DP solution for Constrained Subsequence Sum

--- src/main/java/com/joybean/leetcode/ConstrainedSubsequenceSum.java
+++ src/main/java/com/joybean/leetcode/ConstrainedSubsequenceSum.java
@@ -6,8 +6,28 @@ package com.joybean.leetcode;
  * @author joybean
  */
 public class ConstrainedSubsequenceSum {
-    //TODO
-    public int constrainedSubsetSum(int[] nums, int k) {
-        return 0;
+    /**
+     * DP solution:TLE
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int constrainedSubsetSum1(int[] nums, int k) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i];
+            int n = 1;
+            while (n <= k && i >= n) {
+                dp[i] = Math.max(dp[i], dp[i - n] + nums[i]);
+                n++;
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int n : dp) {
+            max = Math.max(n, max);
+        }
+        return max;
     }
 }
