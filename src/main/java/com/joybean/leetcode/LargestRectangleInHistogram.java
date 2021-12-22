203d2d1c424ce68666cc447a0f7c321473db40fd
joybean <joybeanq@gmail.com>
2021-12-22 15:56:09 +0800

    Another monotonic stack solution for Largest Rectangle in Histogram

--- src/main/java/com/joybean/leetcode/LargestRectangleInHistogram.java
+++ src/main/java/com/joybean/leetcode/LargestRectangleInHistogram.java
@@ -9,13 +9,38 @@ import java.util.Deque;
  * @author Joybean
  */
 public class LargestRectangleInHistogram {
+
     /**
-     * <a href="https://www.cnblogs.com/shepherdgai/p/13551715.html">Monotonic stack</a>
+     * Monotonic stack
      *
      * @param heights
      * @return
      */
     public static int largestRectangleArea1(int[] heights) {
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < heights.length; i++) {
+            //or heights[i] <= heights[stack.peek()]
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                ans = Math.max(ans, height * (i - (stack.isEmpty() ? 0 : stack.peek() + 1)));
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int height = heights[stack.pop()];
+            ans = Math.max(ans, height * (heights.length - (stack.isEmpty() ? 0 : stack.peek() + 1)));
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://www.cnblogs.com/shepherdgai/p/13551715.html">Monotonic stack using sentinel</a>
+     *
+     * @param heights
+     * @return
+     */
+    public static int largestRectangleArea2(int[] heights) {
         int ans = 0;
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(-1);
@@ -34,6 +59,14 @@ public class LargestRectangleInHistogram {
         return ans;
     }
 
+    public static void main(String[] args) {
+        System.out.println(largestRectangleArea1(new int[] {4, 2, 0, 3, 2, 5}));
+        System.out.println(largestRectangleArea1(new int[] {2, 1, 2}));
+        System.out.println(largestRectangleArea1(new int[] {4, 2, 1, 5}));
+        System.out.println(largestRectangleArea1(new int[] {2}));
+        System.out.println(largestRectangleArea1(new int[] {2, 1, 5, 6, 2, 3}));
+    }
+
     /**
      * Segment tree
      * TODO
@@ -41,7 +74,7 @@ public class LargestRectangleInHistogram {
      * @param heights
      * @return
      */
-    public static int largestRectangleArea2(int[] heights) {
+    public static int largestRectangleArea3(int[] heights) {
         return 0;
     }
 
@@ -52,7 +85,7 @@ public class LargestRectangleInHistogram {
      * @param heights
      * @return
      */
-    public static int largestRectangleArea3(int[] heights) {
+    public static int largestRectangleArea4(int[] heights) {
         return 0;
     }
 
