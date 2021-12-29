6723f7f5c216dcef70889c44896468923f18a02b
joybean <joybeanq@gmail.com>
2021-12-29 13:50:40 +0800

    Solution for Binary Search

---
+++ src/main/java/com/joybean/leetcode/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-search/">Binary Search</a>
+ *
+ * @author Joybean
+ */
+public class BinarySearch {
+    /**
+     * Binary search
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            //avoid integer overflow
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
