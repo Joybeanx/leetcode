cfb85080a4b3ef523b7b7c8c81c3a531b3c9e8e2
joybean <joybeanq@gmail.com>
2022-01-06 00:17:34 +0800

    Binary search solution for Find Smallest Letter Greater Than Target

--- src/main/java/com/joybean/leetcode/FindSmallestLetterGreaterThanTarget.java
+++ src/main/java/com/joybean/leetcode/FindSmallestLetterGreaterThanTarget.java
@@ -7,8 +7,28 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FindSmallestLetterGreaterThanTarget {
-    //TODO
-    public char nextGreatestLetter(char[] letters, char target) {
-        return 0;
+    /**
+     * Binary search
+     *
+     * @param letters
+     * @param target
+     * @return
+     */
+    public static char nextGreatestLetter1(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+        //search range [left,right]
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (target >= letters[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (left == letters.length) {
+            return letters[0];
+        }
+        return letters[left];
     }
 }
---
+++ src/main/java/com/joybean/leetcode/SearchInRotatedSortedArray.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/">Search in Rotated Sorted Array</a>
+ *
+ * @author Joybean
+ */
+public class SearchInRotatedSortedArray {
+    //TODO
+    public int search(int[] nums, int target) {
+        return 0;
+    }
+}
