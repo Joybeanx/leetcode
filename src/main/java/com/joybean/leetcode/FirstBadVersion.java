0268677ee8f3c4d02c0bee1856e918d595009c23
joybeanx <ijoybeansang@gmail.com>
2022-01-07 19:38:32 +0800

    Binary search solution for First Bad Version

--- src/main/java/com/joybean/leetcode/FirstBadVersion.java
+++ src/main/java/com/joybean/leetcode/FirstBadVersion.java
@@ -6,8 +6,27 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FirstBadVersion {
-    //TODO
-    public int firstBadVersion(int n) {
-        return 0;
+    /**
+     * Binary search
+     *
+     * @param n
+     * @return
+     */
+    public static int firstBadVersion1(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean isBadVersion(int version) {
+        return false;
     }
 }
