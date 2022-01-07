9ea2b95811d1111001ebc08241174cdb9238eca0
joybeanx <ijoybeansang@gmail.com>
2022-01-07 20:33:59 +0800

    Count sort solution for Find Target Indices After Sorting Array

--- src/main/java/com/joybean/leetcode/FindTargetIndicesAfterSortingArray.java
+++ src/main/java/com/joybean/leetcode/FindTargetIndicesAfterSortingArray.java
@@ -1,5 +1,6 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,8 +10,40 @@ import java.util.List;
  * @author Joybean
  */
 public class FindTargetIndicesAfterSortingArray {
-    //TODO
-    public List<Integer> targetIndices(int[] nums, int target) {
+    /**
+     * Sorting + Binary search
+     * TODO
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static List<Integer> targetIndices1(int[] nums, int target) {
         return null;
+    }
+
+    /**
+     * Counting sort
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static List<Integer> targetIndices2(int[] nums, int target) {
+        int targetCnt = 0;
+        int lessCnt = 0;
+        for (int num : nums) {
+            if (num == target) {
+                targetCnt++;
+            } else if (num < target) {
+                lessCnt++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < targetCnt; i++) {
+            //or ans.add(lessCnt++);
+            ans.add(lessCnt + i);
+        }
+        return ans;
     }
 }
