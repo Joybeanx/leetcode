fa7d8940b80adde25f7479a304c182f8089efbbd
joybean <joybeanq@gmail.com>
2022-01-07 12:12:26 +0800

    Another binary search solution for Find Smallest Letter Greater Than Target

--- src/main/java/com/joybean/leetcode/FindSmallestLetterGreaterThanTarget.java
+++ src/main/java/com/joybean/leetcode/FindSmallestLetterGreaterThanTarget.java
@@ -8,7 +8,7 @@ package com.joybean.leetcode;
  */
 public class FindSmallestLetterGreaterThanTarget {
     /**
-     * Binary search
+     * Binary search 1
      *
      * @param letters
      * @param target
@@ -21,9 +21,36 @@ public class FindSmallestLetterGreaterThanTarget {
         while (left <= right) {
             int mid = (left + right) >>> 1;
             if (letters[mid] <= target) {
+                //Increase the left boundary of search range to locate next possible greater letter
                 left = mid + 1;
             } else {
                 right = mid - 1;
+            }
+        }
+        if (left == letters.length) {
+            return letters[0];
+        }
+        return letters[left];
+    }
+
+    /**
+     * Binary search 2
+     *
+     * @param letters
+     * @param target
+     * @return
+     */
+    public static char nextGreatestLetter2(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length;
+        //search range [left,right)
+        while (left < right) {
+            //Make mid biased to the left,so that this won't make the search range stuck
+            int mid = (left + right - 1) >>> 1;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
         if (left == letters.length) {
