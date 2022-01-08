85468ac7e8dceec61c304de3598ef439fbc6c1e9
joybeanx <ijoybeansang@gmail.com>
2022-01-08 20:46:49 +0800

    More binary search solutions for Search Insert Position

--- src/main/java/com/joybean/leetcode/SearchInsertPosition.java
+++ src/main/java/com/joybean/leetcode/SearchInsertPosition.java
@@ -44,7 +44,7 @@ public class SearchInsertPosition {
         int right = nums.length;
         //search range [left,right)
         while (left < right) {
-            //avoid overflow
+            //Always gives the lower mid,so that this won't make the search range stuck
             int mid = (left + right - 1) >>> 1;
             if (target == nums[mid]) {
                 return mid;
@@ -52,6 +52,59 @@ public class SearchInsertPosition {
             if (nums[mid] < target) {
                 left = mid + 1;
             } else {
+                //mid may be a possible insert position
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/search-insert-position/discuss/249092/Come-on-forget-the-binary-search-patterntemplate!-Try-understand-it!">Binary search 3</a>
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                //right could be the result
+                right = mid;
+            }
+        }
+
+        //We need a post-processing because there is one element left at the end
+        return nums[left] < target ? left + 1 : left;
+    }
+
+
+    /**
+     * <a href="https://leetcode.com/problems/search-insert-position/discuss/423166/Binary-Search-101">Binary search 4</a>
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert4(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        //search range [left,right)
+        while (left < right) {
+            //Always gives the lower mid,so that this won't make the search range stuck
+            int mid = (left + right - 1) >>> 1;
+            //100% sure logic
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                //mid may be a possible insert position
                 right = mid;
             }
         }
