73a7e8441174630ae21d62f0f1f2e88cadbf9c51
joybeanx <ijoybeansang@gmail.com>
2022-01-09 16:50:05 +0800

    Binary search solution for Minimum Number of Days to Make m Bouquets

--- src/main/java/com/joybean/leetcode/MinimumNumberOfDaysToMakeMBouquets.java
+++ src/main/java/com/joybean/leetcode/MinimumNumberOfDaysToMakeMBouquets.java
@@ -6,8 +6,40 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class MinimumNumberOfDaysToMakeMBouquets {
-    //TODO
-    public int minDays(int[] bloomDay, int m, int k) {
-        return 0;
+    /**
+     * <a href="https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/discuss/686316/JavaC%2B%2BPython-Binary-Search">Binary search</a>
+     *
+     * @param bloomDay
+     * @param m
+     * @param k
+     * @return
+     */
+    public static int minDays1(int[] bloomDay, int m, int k) {
+        if (m * k > bloomDay.length) {
+            return -1;
+        }
+        int left = 1;
+        int right = (int) 1e9;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            int bouquets = 0;
+            int bloomed = 0;
+            for (int d : bloomDay) {
+                //Calculate how many bouquets we can make after dth day
+                if (mid < d) {
+                    bloomed = 0;
+                } else if (++bloomed == k) {
+                    bouquets++;
+                    bloomed = 0;
+                }
+            }
+            if (bouquets < m) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return left;
     }
 }
