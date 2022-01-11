7c3976d2237513069448ae0bc13f8c2a672dde00
joybeanx <ijoybeansang@gmail.com>
2022-01-11 10:02:26 +0800

    Binary search solution for Divide Chocolate

--- src/main/java/com/joybean/leetcode/DivideChocolate.java
+++ src/main/java/com/joybean/leetcode/DivideChocolate.java
@@ -6,8 +6,34 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class DivideChocolate {
-    //TODO https://wentao-shao.gitbook.io/leetcode/array/1231.divide-chocolate
-    public int maximizeSweetness(int[] sweetness, int K) {
-        return 0;
+    /**
+     * <a href="https://coordinate.blog.csdn.net/article/details/102646247">Binary search</a>
+     */
+    public static int maximizeSweetness1(int[] sweetness, int k) {
+        int left = Integer.MAX_VALUE;
+        int right = 0;
+        for (int s : sweetness) {
+            left = Math.min(s, left);
+            right += s;
+        }
+        //search range[left,right)
+        while (left < right) {
+            int mid = (left + right + 1) >>> 1;
+            int cuts = 0;
+            int cur = 0;
+            for (int s : sweetness) {
+                cur += s;
+                if (cur >= mid) {
+                    cuts++;
+                    cur = 0;
+                }
+            }
+            if (cuts <= k) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
     }
 }
