89b75e1e824b19edcfe9296b29e4d9b5f8eb2668
joybean <joybeanq@gmail.com>
2022-01-12 23:48:47 +0800

    Binary search solution for Split Array Largest Sum

--- src/main/java/com/joybean/leetcode/SplitArrayLargestSum.java
+++ src/main/java/com/joybean/leetcode/SplitArrayLargestSum.java
@@ -6,8 +6,68 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class SplitArrayLargestSum {
-    //TODO
-    public int splitArray(int[] nums, int m) {
+    /**
+     * Binary search 1
+     *
+     * @param nums
+     * @param m
+     * @return
+     */
+    public static int splitArray1(int[] nums, int m) {
+        int left = Integer.MIN_VALUE;
+        int right = 0;
+        for (int num : nums) {
+            left = Math.max(left, num);
+            right += num;
+        }
+        //search range [left,right)
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (canSplit(nums, m, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean canSplit(int[] nums, int m, int upperBound) {
+        int cnt = 1;
+        int cur = 0;
+        for (int num : nums) {
+            cur += num;
+            if (cur > upperBound) {
+                cur = num;
+                cnt++;
+                if (cnt > m) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Binary search 2
+     *
+     * @param nums
+     * @param m
+     * @return
+     */
+    public static int splitArray2(int[] nums, int m) {
+        return 0;
+    }
+
+    /**
+     * DP
+     * TODO
+     *
+     * @param nums
+     * @param m
+     * @return
+     */
+    public static int splitArray3(int[] nums, int m) {
         return 0;
     }
 }
