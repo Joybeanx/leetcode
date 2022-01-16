238eac2ddaf286fd125a1c8183969cfe4f6c6041
joybean <joybeanq@gmail.com>
2022-01-16 16:05:13 +0800

    Binary search solution for Find Right Interval

--- src/main/java/com/joybean/leetcode/FindRightInterval.java
+++ src/main/java/com/joybean/leetcode/FindRightInterval.java
@@ -1,13 +1,60 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 /**
  * <a href="https://leetcode.com/problems/find-right-interval/">Find Right Interval</a>
  *
  * @author Joybean
  */
 public class FindRightInterval {
-    //TODO
-    public int[] findRightInterval(int[][] intervals) {
+    /**
+     * Binary search
+     *
+     * @param intervals
+     * @return
+     */
+    public static int[] findRightInterval1(int[][] intervals) {
+        Integer[] sortedIndexes = IntStream.range(0, intervals.length).mapToObj(Integer::valueOf).toArray(
+            Integer[]::new);
+        Arrays.sort(sortedIndexes, Comparator.comparing(i -> intervals[i][0]));
+        int[] ans = new int[sortedIndexes.length];
+        for (int i = 0; i < sortedIndexes.length; i++) {
+            ans[i] = binarySearch(i, sortedIndexes, intervals);
+        }
+        return ans;
+    }
+
+    private static int binarySearch(int i, Integer[] sortedIndexes, int[][] intervals) {
+        int left = 0;
+        int right = sortedIndexes.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (intervals[sortedIndexes[mid]][0] == intervals[i][1]) {
+                return sortedIndexes[mid];
+            }
+            if (intervals[sortedIndexes[mid]][0] < intervals[i][1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (left == intervals.length) {
+            return -1;
+        }
+        return sortedIndexes[left];
+    }
+
+    /**
+     * TreeMap
+     * TODO
+     *
+     * @param intervals
+     * @return
+     */
+    public static int[] findRightInterval2(int[][] intervals) {
         return null;
     }
 }
