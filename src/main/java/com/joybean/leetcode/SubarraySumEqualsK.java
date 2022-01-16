588366f798a81b1ec4dce7813b8ac8a3e60d39ac
joybean <joybeanq@gmail.com>
2022-01-16 14:37:11 +0800

    Polish

--- src/main/java/com/joybean/leetcode/SubarraySumEqualsK.java
+++ src/main/java/com/joybean/leetcode/SubarraySumEqualsK.java
@@ -19,7 +19,6 @@ public class SubarraySumEqualsK {
     public static int subarraySum1(int[] nums, int k) {
         //prefixSum[i] represents sum of nums[0..i-1]
         int[] prefixSum = new int[nums.length + 1];
-        prefixSum[0] = 0;
         for (int i = 0; i < nums.length; i++) {
             prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
