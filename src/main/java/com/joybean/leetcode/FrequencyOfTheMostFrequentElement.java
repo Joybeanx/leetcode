4a5bd66e6961a5fea7c8aec243647e88ff692e6a
joybeanx <ijoybeansang@gmail.com>
2022-01-18 23:36:16 +0800

    Add todo

--- src/main/java/com/joybean/leetcode/FrequencyOfTheMostFrequentElement.java
+++ src/main/java/com/joybean/leetcode/FrequencyOfTheMostFrequentElement.java
@@ -7,8 +7,43 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class FrequencyOfTheMostFrequentElement {
-    //TODO
-    public int maxFrequency(int[] nums, int k) {
+    /**
+     * <a href="https://michael.blog.csdn.net/article/details/116130754">Binary search 1</a>
+     * TODO
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int maxFrequency1(int[] nums, int k) {
         return 0;
     }
+//    Wrong binary search
+//    public static int maxFrequency(int[] nums, int k) {
+//        Arrays.sort(nums);
+//        int left = 0;
+//        int right = nums.length - 1;
+//        //search range [left,right)
+//        while (left < right) {
+//            int mid = (left + right + 1) >>> 1;
+//            if (exceedNumOfOperations(nums, mid, k)) {
+//                right = mid - 1;
+//            } else {
+//                left = mid;
+//            }
+//        }
+//        return left + 1;
+//    }
+//
+//    private static boolean exceedNumOfOperations(int[] nums, int targetIdx, int k) {
+//        int cnt = 0;
+//        for (int i = targetIdx - 1; i >= 0; i--) {
+//            cnt += nums[targetIdx] - nums[i];
+//            if (cnt > k) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
 }
