0ad28dff1c38130a0caf01d10a8be114c4bed347
joybean <joybeanq@gmail.com>
2022-01-19 15:04:19 +0800

    Binary search solution for Frequency of the Most Frequent Element

--- src/main/java/com/joybean/leetcode/FrequencyOfTheMostFrequentElement.java
+++ src/main/java/com/joybean/leetcode/FrequencyOfTheMostFrequentElement.java
@@ -1,5 +1,7 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/frequency-of-the-most-frequent-element/">Frequency of the Most Frequent
  * Element</a>
@@ -8,42 +10,81 @@ package com.joybean.leetcode;
  */
 public class FrequencyOfTheMostFrequentElement {
     /**
-     * <a href="https://michael.blog.csdn.net/article/details/116130754">Binary search 1</a>
-     * TODO
+     * <a href="https://michael.blog.csdn.net/article/details/116130754">Prefix sum + Binary search</a>
      *
      * @param nums
      * @param k
      * @return
      */
     public static int maxFrequency1(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 1;
+        int right = nums.length;
+        //int array is not enough,max:10^5*10^5
+        long[] prefixSum = new long[nums.length + 1];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+        //search range [left,right)
+        while (left < right) {
+            int mid = (left + right + 1) >>> 1;
+            //Can k operations be feasible when max frequency is mid?
+            if (isFeasible(nums, prefixSum, mid, k)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean isFeasible(int[] nums, long[] prefixSum, int freq, int k) {
+        for (int i = freq; i < prefixSum.length; i++) {
+            if (prefixSum[i] - prefixSum[i - freq] + k >= nums[i - 1] * (long)freq) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Sliding window
+     * TODO
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int maxFrequency2(int[] nums, int k) {
         return 0;
     }
-//    Wrong binary search
-//    public static int maxFrequency(int[] nums, int k) {
-//        Arrays.sort(nums);
-//        int left = 0;
-//        int right = nums.length - 1;
-//        //search range [left,right)
-//        while (left < right) {
-//            int mid = (left + right + 1) >>> 1;
-//            if (exceedNumOfOperations(nums, mid, k)) {
-//                right = mid - 1;
-//            } else {
-//                left = mid;
-//            }
-//        }
-//        return left + 1;
-//    }
-//
-//    private static boolean exceedNumOfOperations(int[] nums, int targetIdx, int k) {
-//        int cnt = 0;
-//        for (int i = targetIdx - 1; i >= 0; i--) {
-//            cnt += nums[targetIdx] - nums[i];
-//            if (cnt > k) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
+
+    //    Wrong binary search
+    //    public static int maxFrequency(int[] nums, int k) {
+    //        Arrays.sort(nums);
+    //        int left = 0;
+    //        int right = nums.length - 1;
+    //        //search range [left,right)
+    //        while (left < right) {
+    //            int mid = (left + right + 1) >>> 1;
+    //            if (exceedNumOfOperations(nums, mid, k)) {
+    //                right = mid - 1;
+    //            } else {
+    //                left = mid;
+    //            }
+    //        }
+    //        return left + 1;
+    //    }
+    //
+    //    private static boolean exceedNumOfOperations(int[] nums, int targetIdx, int k) {
+    //        int cnt = 0;
+    //        for (int i = targetIdx - 1; i >= 0; i--) {
+    //            cnt += nums[targetIdx] - nums[i];
+    //            if (cnt > k) {
+    //                return true;
+    //            }
+    //        }
+    //        return false;
+    //    }
 
 }
