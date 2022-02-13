3e1916425a9ab142b5a56472d212c26332fc712f
joybeanx <ijoybeansang@gmail.com>
2022-02-13 16:47:18 +0800

    Sliding window solution for Count Number of Nice Subarrays

--- src/main/java/com/joybean/leetcode/CountNumberOfNiceSubarrays.java
+++ src/main/java/com/joybean/leetcode/CountNumberOfNiceSubarrays.java
@@ -6,8 +6,32 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class CountNumberOfNiceSubarrays {
-    //TODO
-    public int numberOfSubarrays(int[] nums, int k) {
-        return 0;
+    /**
+     * Sliding window
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private static int atMost(int[] nums, int k) {
+        int ans = 0;
+        int left = 0;
+        int odds = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if ((nums[right] & 1) == 1) {
+                odds++;
+            }
+            while (odds > k) {
+                if ((nums[left++] & 1) == 1) {
+                    odds--;
+                }
+            }
+            ans += right - left + 1;
+        }
+        return ans;
     }
 }
