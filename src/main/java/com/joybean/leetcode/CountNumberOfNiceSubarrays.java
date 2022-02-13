cc51eab45abb9c0549f28768d178768072b726a9
joybeanx <ijoybeansang@gmail.com>
2022-02-13 22:37:32 +0800

    One-pass sliding window solution for Count Number of Nice Subarrays

--- src/main/java/com/joybean/leetcode/CountNumberOfNiceSubarrays.java
+++ src/main/java/com/joybean/leetcode/CountNumberOfNiceSubarrays.java
@@ -6,14 +6,27 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class CountNumberOfNiceSubarrays {
+
     /**
-     * Sliding window
+     * <a href="https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/419483/Subarray-Sum-Equals-K">Prefix sum</a>
+     * TODO
      *
      * @param nums
      * @param k
      * @return
      */
-    public static int numberOfSubarrays(int[] nums, int k) {
+    public static int numberOfSubarrays1(int[] nums, int k) {
+        return 0;
+    }
+
+    /**
+     * Two-pass sliding window
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int numberOfSubarrays2(int[] nums, int k) {
         return atMost(nums, k) - atMost(nums, k - 1);
     }
 
@@ -22,15 +35,49 @@ public class CountNumberOfNiceSubarrays {
         int left = 0;
         int odds = 0;
         for (int right = 0; right < nums.length; right++) {
-            if ((nums[right] & 1) == 1) {
-                odds++;
-            }
+            odds += nums[right] & 1;
             while (odds > k) {
-                if ((nums[left++] & 1) == 1) {
-                    odds--;
-                }
+                odds -= nums[left++] & 1;
             }
             ans += right - left + 1;
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/508217/C%2B%2B%3A-Visual-explanation.-O(1)-space.-Two-pointers">One-pass sliding window 1</a>
+     * TODO
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int numberOfSubarrays3(int[] nums, int k) {
+        return 0;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/419378/JavaC%2B%2BPython-Sliding-Window-O(1)-Space">One-pass sliding window 2</a>
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int numberOfSubarrays4(int[] nums, int k) {
+        int ans = 0;
+        int left = 0;
+        int odds = 0;
+        int count = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if ((nums[right] & 1) == 1) {
+                odds++;
+                count = 0;
+            }
+            while (odds == k) {
+                odds -= nums[left++] & 1;
+                count++;
+            }
+            ans += count;
         }
         return ans;
     }
