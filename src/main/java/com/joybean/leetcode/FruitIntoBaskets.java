75fe3041d2e67b3f0741f44d3a4e0553aebc7093
joybean <joybeanq@gmail.com>
2022-02-20 21:32:25 +0800

    Add comment

--- src/main/java/com/joybean/leetcode/FruitIntoBaskets.java
+++ src/main/java/com/joybean/leetcode/FruitIntoBaskets.java
@@ -74,6 +74,7 @@ public class FruitIntoBaskets {
         Map<Integer, Integer> map = new HashMap<>();
         for (right = 0; right < fruits.length; right++) {
             map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+            //see explanation by xingHong
             if (map.size() > 2) {
                 map.put(fruits[left], map.get(fruits[left]) - 1);
                 map.remove(fruits[left++], 0);
