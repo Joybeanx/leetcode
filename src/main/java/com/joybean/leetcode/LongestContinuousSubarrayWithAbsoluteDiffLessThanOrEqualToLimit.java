91e0fedd3c6e831cf35c6e6aa01764e7bf432ae6
joybean <joybeanq@gmail.com>
2022-02-23 23:46:12 +0800

    Sliding window solution for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- src/main/java/com/joybean/leetcode/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
+++ src/main/java/com/joybean/leetcode/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -1,15 +1,72 @@
 package com.joybean.leetcode;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <a href="https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/">Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit</a>
+ * <a href="https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/">Longest
+ * Continuous Subarray With Absolute Diff Less Than or Equal to Limit</a>
  *
  * @author Joybean
  */
 public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
-    //TODO
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        return null;
+    /**
+     * Sliding window
+     *
+     * @param nums
+     * @param limit
+     * @return
+     */
+    public static int longestSubarray1(int[] nums, int limit) {
+        int left = 0;
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        int ans = 0;
+        Map<Integer, Integer> windowCounts = new HashMap<>();
+        for (int right = 0; right < nums.length; right++) {
+            min = Math.min(nums[right], min);
+            max = Math.max(nums[right], max);
+            windowCounts.put(nums[right], windowCounts.getOrDefault(nums[right], 0) + 1);
+            while (max - min > limit) {
+                windowCounts.put(nums[left], windowCounts.getOrDefault(nums[left], 0) - 1);
+                if (windowCounts.remove(nums[left], 0)) {
+                    if (nums[left] == min) {
+                        min =
+                            windowCounts.keySet().stream().mapToInt(Integer::intValue).min().orElse(0);
+                    } else if (nums[left] == max) {
+                        max = windowCounts.keySet().stream().mapToInt(Integer::intValue).max().orElse(0);
+                    }
+                }
+                left++;
+            }
+            ans = Math.max(right - left + 1, ans);
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to
+     * -limit/discuss/609771/JavaC%2B%2BPython-Deques-O(N)">Sliding window with two dequeues</a>
+     * TODO
+     *
+     * @param nums
+     * @param limit
+     * @return
+     */
+    public static int longestSubarray2(int[] nums, int limit) {
+        return 0;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to
+     * -limit/discuss/609771/JavaC%2B%2BPython-Deques-O(N)">TreeMap</a>
+     * TODO
+     *
+     * @param nums
+     * @param limit
+     * @return
+     */
+    public static int longestSubarray3(int[] nums, int limit) {
+        return 0;
     }
 }
