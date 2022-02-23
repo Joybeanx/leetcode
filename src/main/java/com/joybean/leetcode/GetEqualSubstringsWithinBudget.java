4eb85095a54949caf6c1b096c98ee21a9baeb6a6
joybean <joybeanq@gmail.com>
2022-02-23 11:58:14 +0800

    Sliding window solution for Get Equal Substring Within Budget

--- src/main/java/com/joybean/leetcode/GetEqualSubstringsWithinBudget.java
+++ src/main/java/com/joybean/leetcode/GetEqualSubstringsWithinBudget.java
@@ -6,8 +6,25 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class GetEqualSubstringsWithinBudget {
-    //TODO
-    public int equalSubstring(String s, String t, int maxCost) {
-        return 0;
+    /**
+     * Sliding window
+     *
+     * @param s
+     * @param t
+     * @param maxCost
+     * @return
+     */
+    public static int equalSubstring1(String s, String t, int maxCost) {
+        int left = 0;
+        int sum = 0;
+        int ans = 0;
+        for (int right = 0; right < s.length(); right++) {
+            sum += Math.abs(t.charAt(right) - s.charAt(right));
+            while (sum > maxCost) {
+                sum -= Math.abs(t.charAt(left) - s.charAt(left++));
+            }
+            ans = Math.max(right - left + 1, ans);
+        }
+        return ans;
     }
 }
