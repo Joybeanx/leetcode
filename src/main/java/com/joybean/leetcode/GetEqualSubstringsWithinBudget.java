0db0aea93be78df44d9fb94491791ada1a65be35
joybean <joybeanq@gmail.com>
2022-02-24 21:23:57 +0800

    Optimized sliding window solution for Get Equal Substring Within Budget

--- src/main/java/com/joybean/leetcode/GetEqualSubstringsWithinBudget.java
+++ src/main/java/com/joybean/leetcode/GetEqualSubstringsWithinBudget.java
@@ -27,4 +27,29 @@ public class GetEqualSubstringsWithinBudget {
         }
         return ans;
     }
+
+    /**
+     * <a href="https://leetcode.com/problems/get-equal-substrings-within-budget/discuss/392837/JavaC%2B%2BPython
+     * -Sliding-Window">Optimized sliding window</a>
+     *
+     * @param s
+     * @param t
+     * @param maxCost
+     * @return
+     */
+    public static int equalSubstring2(String s, String t, int maxCost) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        for (right = 0; right < s.length(); right++) {
+            sum += Math.abs(t.charAt(right) - s.charAt(right));
+            if (sum > maxCost) {
+                sum -= Math.abs(t.charAt(left) - s.charAt(left++));
+            }
+            // Possible result (the length of [left,right]) is always increasing, we don't need to do calculation
+            // everytime. see zywscq's explanation.
+        }
+        //right = s.length
+        return right - left;
+    }
 }
