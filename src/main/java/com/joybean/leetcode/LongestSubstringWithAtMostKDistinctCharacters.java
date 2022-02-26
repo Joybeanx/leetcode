4e900d1b3f7ff8d37ba6893cd9c3c04ec32e0648
joybean <joybeanq@gmail.com>
2022-02-26 16:32:28 +0800

    Add sliding window problem

---
+++ src/main/java/com/joybean/leetcode/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -0,0 +1,14 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/">Longest Substring with
+ * At Most K Distinct Characters</a>
+ *
+ * @author Joybean
+ */
+public class LongestSubstringWithAtMostKDistinctCharacters {
+    //TODO https://www.lintcode.com/problem/386/
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        return 0;
+    }
+}
