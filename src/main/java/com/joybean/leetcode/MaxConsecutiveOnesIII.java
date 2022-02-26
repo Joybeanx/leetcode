1abb1d9fb6d7385502ab423a49daf0498359c626
joybean <joybeanq@gmail.com>
2022-02-26 16:28:18 +0800

    Sliding window solutions for Max Consecutive Ones III

--- src/main/java/com/joybean/leetcode/MaxConsecutiveOnesIII.java
+++ src/main/java/com/joybean/leetcode/MaxConsecutiveOnesIII.java
@@ -6,8 +6,48 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class MaxConsecutiveOnesIII {
-    //TODO
-    public int longestOnes(int[] nums, int k) {
-        return 0;
+    /**
+     * Sliding window 1
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int longestOnes1(int[] nums, int k) {
+        int left = 0;
+        int ans = 0;
+        int zeroCnt = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCnt++;
+            }
+            while (zeroCnt > k && nums[left++] == 0) {
+                zeroCnt--;
+            }
+            ans = Math.max(right - left + 1, ans);
+        }
+        return ans;
+    }
+
+    /**
+     * Sliding window 2
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int longestOnes2(int[] nums, int k) {
+        int left = 0;
+        int zeroCnt = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCnt++;
+            }
+            //If we find a feasible window, we use this window size to iterate until we find a larger one(if any).
+            if (zeroCnt > k && nums[left++] == 0) {
+                zeroCnt--;
+            }
+        }
+        return nums.length - left;
     }
 }
