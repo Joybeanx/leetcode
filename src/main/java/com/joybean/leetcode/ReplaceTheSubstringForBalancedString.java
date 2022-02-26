fe1f9e6a58acee2127c5fc86a8b51af6ea5bef7d
joybean <joybeanq@gmail.com>
2022-02-26 23:40:21 +0800

    New sliding window solution for Replace the Substring for Balanced String

--- src/main/java/com/joybean/leetcode/ReplaceTheSubstringForBalancedString.java
+++ src/main/java/com/joybean/leetcode/ReplaceTheSubstringForBalancedString.java
@@ -11,8 +11,7 @@ import java.util.Map;
  */
 public class ReplaceTheSubstringForBalancedString {
     /**
-     * <a href="https://leetcode.com/problems/replace-the-substring-for-balanced-string/discuss/408978/JavaC%2B
-     * %2BPython-Sliding-Window">Sliding window</a>
+     * Sliding window 1
      *
      * @param s
      * @return
@@ -28,10 +27,36 @@ public class ReplaceTheSubstringForBalancedString {
         int ans = s.length();
         for (int right = 0; right < s.length(); right++) {
             countMap.merge(s.charAt(right), -1, Integer::sum);
-            //We can make s balanced as long as max(count[Q],count[W],count[E],count[R]) <= n / 4.
+            //We can make s balanced as long as all the counts of characters <= n / 4.
             while (left < s.length() && countMap.values().stream().allMatch(each -> each <= expectedCnt)) {
                 ans = Math.min(right - left + 1, ans);
                 countMap.merge(s.charAt(left++), 1, Integer::sum);
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/replace-the-substring-for-balanced-string/discuss/408978/JavaC%2B
+     * %2BPython-Sliding-Window">Sliding window 2</a>
+     *
+     * @param s
+     * @return
+     */
+    public static int balancedString2(String s) {
+        int[] count = new int[128];
+        int n = s.length();
+        int ans = s.length();
+        int left = 0;
+        int k = n / 4;
+        for (Character ch : s.toCharArray()) {
+            count[ch]++;
+        }
+        for (int right = 0; right < n; right++) {
+            --count[s.charAt(right)];
+            while (left < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+                ans = Math.min(right - left + 1, ans);
+                ++count[s.charAt(left++)];
             }
         }
         return ans;
