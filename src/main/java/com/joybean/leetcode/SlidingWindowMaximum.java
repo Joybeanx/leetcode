a4b0aabce1ee9c69b89a92c29044e27e9bfafb9d
joybean <joybeanq@gmail.com>
2022-02-28 23:49:57 +0800

    Sliding window solution for Sliding Window Maximum

--- src/main/java/com/joybean/leetcode/SlidingWindowMaximum.java
+++ src/main/java/com/joybean/leetcode/SlidingWindowMaximum.java
@@ -1,13 +1,56 @@
 package com.joybean.leetcode;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/sliding-window-maximum/">Sliding Window Maximum</a>
  *
  * @author joybean
  */
 public class SlidingWindowMaximum {
-    //TODO
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    /**
+     * Monotonic queue
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow1(int[] nums, int k) {
+        int left = 0;
+        int[] ans = new int[nums.length - k + 1];
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Deque<Integer> deque = new LinkedList<>();
+        int index = 0;
+        for (int right = 0; right < nums.length; right++) {
+            countMap.merge(nums[right], 1, Integer::sum);
+            while (!deque.isEmpty() && deque.peekLast() <= nums[right]) {
+                deque.pollLast();
+            }
+            deque.offerLast(nums[right]);
+            if (right - left + 1 == k) {
+                ans[index++] = deque.peek();
+                countMap.merge(nums[left], -1, Integer::sum);
+                if (countMap.remove(nums[left], 0) && nums[left] == deque.peek()) {
+                    deque.poll();
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/sliding-window-maximum/discuss/65884/Java-O(n)
+     * -solution-using-deque-with-explanation">Optimized sliding window</a>
+     * TODO
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
         return null;
     }
 }
