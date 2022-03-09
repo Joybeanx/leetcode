b8029eaf2b68152f79a2f3278d7c8800b35d108f
joybean <joybeanq@gmail.com>
2022-03-09 14:38:06 +0800

    Polish

--- src/main/java/com/joybean/leetcode/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
+++ src/main/java/com/joybean/leetcode/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -54,9 +54,9 @@ public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
      * @return
      */
     public static int longestSubarray2(int[] nums, int limit) {
-        //The head of maxDeque is always the max value in the window [left,right]
+        //The head of maxDeque is always the max value in window [left,right]
         Deque<Integer> maxDeque = new LinkedList<>();
-        //The head of minDeque is always the min value in the window [left,right]
+        //The head of minDeque is always the min value in window [left,right]
         Deque<Integer> minDeque = new LinkedList<>();
         int left = 0;
         int ans = 0;
