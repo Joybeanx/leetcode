f223bbbb7c868eef29c83c29004aa96d69b9214d
joybean <joybeanq@gmail.com>
2022-04-13 12:11:05 +0800

    Backtraking solution for Combinations

--- src/main/java/com/joybean/leetcode/Combinations.java
+++ src/main/java/com/joybean/leetcode/Combinations.java
@@ -1,5 +1,6 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,8 +9,31 @@ import java.util.List;
  * @author Joybean
  */
 public class Combinations {
-    //TODO
-    public List<List<Integer>> combine(int n, int k) {
-        return null;
+    /**
+     * backtracking
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public static List<List<Integer>> combine1(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(ans, n, k, 1, new ArrayList<>());
+        return ans;
+    }
+
+    private static void backtrack(List<List<Integer>> ans, int n, int k, int start, List<Integer> curPath) {
+        if (curPath.size() == k) {
+            ans.add(new ArrayList<>(curPath));
+            return;
+        }
+        if (start > n) {
+            return;
+        }
+        for (int cur = start; cur <= n; cur++) {
+            curPath.add(cur);
+            backtrack(ans, n, k, cur + 1, curPath);
+            curPath.remove(curPath.size() - 1);
+        }
     }
 }
