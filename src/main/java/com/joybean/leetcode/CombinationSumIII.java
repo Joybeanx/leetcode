e4746a3046dec3cb1ed8661c52ba74c83cfa0e57
joybean <joybeanq@gmail.com>
2022-04-23 13:11:37 +0800

    Backtracking solution for Combination Sum III

--- src/main/java/com/joybean/leetcode/CombinationSumIII.java
+++ src/main/java/com/joybean/leetcode/CombinationSumIII.java
@@ -1,5 +1,6 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,8 +9,36 @@ import java.util.List;
  * @author Joybean
  */
 public class CombinationSumIII {
-    //TODO
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        return null;
+    /**
+     * backtracking
+     *
+     * @param k
+     * @param n
+     * @return
+     */
+    public static List<List<Integer>> combinationSum1(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack1(k, n, 1, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private static void backtrack1(int k, int target, int start, List<Integer> curPath, List<List<Integer>> ans) {
+        if (curPath.size() == k) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(curPath));
+            }
+            return;
+        }
+
+        for (int cur = start; cur <= 9; cur++) {
+            int newTarget = target - cur;
+            //pruning
+            if (curPath.size() == k - 1 && newTarget < 0) {
+                break;
+            }
+            curPath.add(cur);
+            backtrack1(k, newTarget, cur + 1, curPath, ans);
+            curPath.remove(curPath.size() - 1);
+        }
     }
 }
