e331f8f5385ce6b4237acc1c69ae6d3326652fea
joybean <joybeanq@gmail.com>
2022-04-27 00:42:19 +0800

    BFS solution for Minimum Depth of Binary Tree

--- src/main/java/com/joybean/leetcode/MinimumDepthOfBinaryTree.java
+++ src/main/java/com/joybean/leetcode/MinimumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package com.joybean.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * <a href="https://leetcode.com/problems/minimum-depth-of-binary-tree/">Minimum Depth of Binary Tree</a>
  *
@@ -56,13 +59,34 @@ public class MinimumDepthOfBinaryTree {
 
     /**
      * BFS
-     * TODO
      *
      * @param root
      * @return
      */
     public static int minDepth3(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null) {
+                    return depth;
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 
     public static class TreeNode {
@@ -72,7 +96,7 @@ public class MinimumDepthOfBinaryTree {
 
         TreeNode() {}
 
-        TreeNode(int val) { this.val = val; }
+        TreeNode(int val) {this.val = val;}
 
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
