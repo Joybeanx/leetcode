e9f60222a18a35f3d38d2974dd4097981a062677
joybean <joybeanq@gmail.com>
2022-05-01 23:30:03 +0800

    Pancake Sorting

---
+++ src/main/java/com/joybean/leetcode/PancakeSorting.java
@@ -0,0 +1,15 @@
+package com.joybean.leetcode;
+
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/pancake-sorting/">Pancake Sorting</a>
+ *
+ * @author Joybean
+ */
+public class PancakeSorting {
+    //TODO
+    public List<Integer> pancakeSort(int[] arr) {
+        return null;
+    }
+}
