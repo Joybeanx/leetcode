db34e327d1e81961a8f17c406bd611792c4b781b
joybean <joybeanq@gmail.com>
2022-05-01 15:02:34 +0800

    Two pointers solutions for 3Sum

--- src/main/java/com/joybean/leetcode/ThreeSum.java
+++ src/main/java/com/joybean/leetcode/ThreeSum.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * <a href="https://leetcode.com/problems/3sum/">3Sum</a>
@@ -23,7 +24,7 @@ public class ThreeSum {
      */
     public static List<List<Integer>> threeSum1(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             Map<Integer, List<Integer>> map = new HashMap<>();
             for (int j = i + 1; j < nums.length; j++) {
                 List<Integer> list;
@@ -44,23 +45,50 @@ public class ThreeSum {
     }
 
     /**
-     * Sort and Two pointers
+     * Two pointers using set to avoid duplicates:concise but very slow
      *
      * @param nums
      * @return
      */
-    public List<List<Integer>> threeSum2(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return res;
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Set<List<Integer>> ans = new HashSet<>();
+        if (nums.length < 3) {
+            return new ArrayList<>(ans);
         }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                } else if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum3(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
             // Skip equal elements to avoid duplicates
             if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left = i + 1, right = nums.length - 1;
+            int left = i + 1;
+            int right = nums.length - 1;
             // Two Pointers
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
@@ -84,5 +112,43 @@ public class ThreeSum {
             }
         }
         return res;
+    }
+
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum4(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+            while (left < right) {
+                int twoSum = nums[left] + nums[right];
+                if (twoSum == target) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (twoSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
     }
 }
