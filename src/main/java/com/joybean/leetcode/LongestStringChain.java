c170483995467633c21d762e11e724419bedb8a6
joybean <joybeanq@gmail.com>
2022-05-01 21:57:44 +0800

    Longest String Chain

---
+++ src/main/java/com/joybean/leetcode/LongestStringChain.java
@@ -0,0 +1,20 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-string-chain/">Longest String Chain</a>
+ *
+ * @author Joybean
+ */
+public class LongestStringChain {
+
+
+    /**
+     * DP
+     * //TODO
+     * @param words
+     * @return
+     */
+    public int longestStrChain(String[] words) {
+        return 0;
+    }
+}
