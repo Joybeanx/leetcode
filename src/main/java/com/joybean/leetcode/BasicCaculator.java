6fbb8ac490e0093930b1ea63525929f422aca998
joybean <joybeanq@gmail.com>
2022-05-01 23:09:34 +0800

    Basic Calculator

---
+++ src/main/java/com/joybean/leetcode/BasicCaculator.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/basic-calculator/">Basic Calculator</a>
+ *
+ * @author Joybean
+ */
+public class BasicCaculator {
+    //TODO
+    public int calculate(String s) {
+        return 0;
+    }
+}
