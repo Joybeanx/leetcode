fc0e6939f6b2a037d2fd76dbbd31727c41b0688f
joybean <joybeanq@gmail.com>
2022-05-03 12:06:26 +0800

    Exam Room

---
+++ src/main/java/com/joybean/leetcode/ExamRoom.java
@@ -0,0 +1,19 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/exam-room/">Exam Room</a>
+ *
+ * @author Joybean
+ */
+public class ExamRoom {
+    //TODO
+    public ExamRoom(int n) {
+    }
+
+    public int seat() {
+        return 0;
+    }
+
+    public void leave(int p) {
+    }
+}
