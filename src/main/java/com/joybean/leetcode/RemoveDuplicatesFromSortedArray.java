b4d7fa0033ac1d270a889f7e5b9446bf79fe8860
joybean <joybeanq@gmail.com>
2022-05-05 16:55:46 +0800

    Solutions for Remove Duplicates from Sorted Array

--- src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -6,8 +6,54 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RemoveDuplicatesFromSortedArray {
-    //TODO
-    public int removeDuplicates(int[] nums) {
-        return 0;
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates1(int[] nums) {
+        int left = 0;
+        int right = 1;
+        while (right < nums.length) {
+            if (nums[right] != nums[left]) {
+                nums[++left] = nums[right];
+            }
+            right++;
+        }
+        return ++left;
+    }
+
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates2(int[] nums) {
+        int left = 0;
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[right] != nums[left]) {
+                nums[++left] = nums[right];
+            }
+        }
+        return ++left;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/discuss/11780/5-lines-C%2B%2BJava
+     * -nicer-loops">Nicer loop</a>
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates3(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (num != nums[i]) {
+                nums[++i] = num;
+            }
+        }
+        return ++i;
     }
 }
