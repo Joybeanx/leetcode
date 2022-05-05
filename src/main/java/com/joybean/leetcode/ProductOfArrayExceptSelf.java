d047de75b46f499d1b7a8b62da34471f996d80c7
joybean <joybeanq@gmail.com>
2022-05-05 14:19:51 +0800

    Prefix & Suffix product solution for Product of Array Except Self

--- src/main/java/com/joybean/leetcode/ProductOfArrayExceptSelf.java
+++ src/main/java/com/joybean/leetcode/ProductOfArrayExceptSelf.java
@@ -6,8 +6,24 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class ProductOfArrayExceptSelf {
-    //TODO
-    public int[] productExceptSelf(int[] nums) {
-        return null;
+    /**
+     * Prefix & Suffix product
+     *
+     * @param nums
+     * @return
+     */
+    public static int[] productExceptSelf1(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //ans[i] represents the product of nums[0...i-1]
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
     }
 }
