237e585c713b7ff8f594983b59411fb9738615c0
joybean <joybeanq@gmail.com>
2022-05-05 12:13:29 +0800

    Add Find the Student that Will Replace the Chalk

---
+++ src/main/java/com/joybean/leetcode/FindTheStudentThatWillReplaceTheChalk.java
@@ -0,0 +1,21 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/">Find the Student that Will
+ * Replace the Chalk</a>
+ *
+ * @author Joybean
+ */
+public class FindTheStudentThatWillReplaceTheChalk {
+
+    /**
+     * Prefix sum
+     * //TODO
+     * @param chalk
+     * @param k
+     * @return
+     */
+    public int chalkReplacer(int[] chalk, int k) {
+        return 0;
+    }
+}
