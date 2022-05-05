a364c47d7fb5d20f7d877a93d00f3557408368ca
joybean <joybeanq@gmail.com>
2022-05-05 17:18:36 +0800

    Solutions for Remove Element

--- src/main/java/com/joybean/leetcode/RemoveElement.java
+++ src/main/java/com/joybean/leetcode/RemoveElement.java
@@ -6,8 +6,37 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RemoveElement {
-    //TODO
-    public int removeElement(int[] nums, int val) {
-        return 0;
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement1(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+
+    /**
+     * Nicer loop
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement2(int[] nums, int val) {
+        int i = 0;
+        for (int num : nums) {
+            if (num != val) {
+                nums[i++] = num;
+            }
+        }
+        return i;
     }
 }
