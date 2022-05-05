f5ee9b33e29c4932be477bda04e4689aa238f470
joybean <joybeanq@gmail.com>
2022-05-05 18:56:40 +0800

    Another iterative solution for Remove Duplicates from Sorted List II

--- src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedList2.java
+++ src/main/java/com/joybean/leetcode/RemoveDuplicatesFromSortedList2.java
@@ -24,11 +24,41 @@ public class RemoveDuplicatesFromSortedList2 {
                 do {
                     cur = cur.next;
                 } while (cur.next != null && cur.val == cur.next.val);
-                // skip all duplicates
+                // skip all duplicates, but shouldn't move predecessor
                 pred.next = cur.next;
             } else {
                 //move predecessor
                 pred = pred.next;
+            }
+            cur = cur.next;
+        }
+        return sentinel.next;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28335/My-accepted-Java-code">Iterative
+     * solution 2</a>
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates2(ListNode head) {
+        ListNode sentinel = new ListNode(0, head);
+        ListNode cur = head;
+        //the last node before the sublist of duplicates
+        ListNode pred = sentinel;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                // move till the end of duplicates sublist
+                cur = cur.next;
+            }
+            //cur.val is distinct
+            if (pred.next == cur) {
+                //move predecessor
+                pred = pred.next;
+            } else {
+                //skip all duplicates, but shouldn't move predecessor
+                pred.next = cur.next;
             }
             cur = cur.next;
         }
@@ -41,18 +71,18 @@ public class RemoveDuplicatesFromSortedList2 {
      * @param head
      * @return
      */
-    public static ListNode deleteDuplicates2(ListNode head) {
+    public static ListNode deleteDuplicates3(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
         if (head.val != head.next.val) {
-            head.next = deleteDuplicates2(head.next);
+            head.next = deleteDuplicates3(head.next);
             return head;
         }
         do {
             head = head.next;
         } while (head.next != null && head.val == head.next.val);
-        return deleteDuplicates2(head.next);
+        return deleteDuplicates3(head.next);
     }
 
     public static class ListNode {
@@ -61,7 +91,7 @@ public class RemoveDuplicatesFromSortedList2 {
 
         ListNode() {}
 
-        ListNode(int val) { this.val = val; }
+        ListNode(int val) {this.val = val;}
 
         ListNode(int val, ListNode next) {
             this.val = val;
