8c5095df6e9b3e0f86a097d7ed36dd8a385f0c45
joybean <joybeanq@gmail.com>
2022-05-06 23:36:02 +0800

    Add Subarray Product Less Than K

---
+++ src/main/java/com/joybean/leetcode/SubarrayProductLessThanK.java
@@ -0,0 +1,21 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/subarray-product-less-than-k/">Subarray Product Less Than K</a>
+ *
+ * @author Joybean
+ */
+public class SubarrayProductLessThanK {
+
+
+    /**
+     * Sliding window
+     * //TODO
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        return 0;
+    }
+}
