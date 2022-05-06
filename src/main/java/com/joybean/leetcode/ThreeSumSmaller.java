8c0241fb400ca220e523b6bab49c7378a4163627
joybean <joybeanq@gmail.com>
2022-05-06 16:48:59 +0800

    Two pointers solution for 3Sum Smaller

--- src/main/java/com/joybean/leetcode/ThreeSumSmaller.java
+++ src/main/java/com/joybean/leetcode/ThreeSumSmaller.java
@@ -1,5 +1,7 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/3sum-smaller/">3Sum Smaller</a>
  *
@@ -8,13 +10,32 @@ package com.joybean.leetcode;
 public class ThreeSumSmaller {
     /**
      * <a href="https://www.lintcode.com/problem/918/">Two Pointers</a>
-     * TODO
      *
      * @param nums
      * @param target
      * @return
      */
     public int threeSumSmaller(int[] nums, int target) {
-        return 0;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int curSum = nums[i] + nums[left] + nums[right];
+                if (curSum < target) {
+                    //valid triplets: (nums[i],nums[left],nums[left+1...right])
+                    ans += right - left;
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
     }
 }
