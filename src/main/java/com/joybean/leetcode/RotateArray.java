ccf20dd4aeca6e0cd8434dbf42c0e55f69c36a3c
joybean <joybeanq@gmail.com>
2022-05-06 12:36:39 +0800

    Two pointer solution for Rotate Array

--- src/main/java/com/joybean/leetcode/RotateArray.java
+++ src/main/java/com/joybean/leetcode/RotateArray.java
@@ -6,8 +6,39 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RotateArray {
-    //TODO
-    public void rotate(int[] nums, int k) {
+    /**
+     * Two pointers
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate1(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        int tmp = 0;
+        while (start < end) {
+            tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/rotate-array/discuss/54282/My-interpretationproof-of-the-Cyclic
+     * -Replacements-Method-in-Editorial-Solution">Cyclic Replacements</a>
+     * TODO
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate2(int[] nums, int k) {
 
     }
 }
