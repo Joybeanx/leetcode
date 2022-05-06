c9f3ac7869960a58e1be34294228d3a584dc6120
joybean <joybeanq@gmail.com>
2022-05-06 23:16:13 +0800

    Two pointers solution for Two Sum Less Than K

--- src/main/java/com/joybean/leetcode/TwoSumLessThanK.java
+++ src/main/java/com/joybean/leetcode/TwoSumLessThanK.java
@@ -1,5 +1,7 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://wentao-shao.gitbook.io/leetcode/two-pointers/1099.two-sum-less-than-k/">Two Sum Less Than K</a>
  *
@@ -8,13 +10,26 @@ package com.joybean.leetcode;
 public class TwoSumLessThanK {
     /**
      * <a href="https://lixinchengdu.github.io/algorithmbook/leetcode/two-sum-less-than-k.html">Two pointers</a>
-     * TODO
      *
-     * @param A
-     * @param K
+     * @param nums
+     * @param k
      * @return
      */
-    public int twoSumLessThanK(int[] A, int K) {
-        return 0;
+    public static int twoSumLessThanK1(int[] nums, int k) {
+        int ans = -1;
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum >= k) {
+                right--;
+            } else {
+                ans = Math.max(sum, ans);
+                left++;
+            }
+
+        }
+        return ans;
     }
 }
