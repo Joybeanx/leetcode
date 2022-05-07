d4c1842083bc3189a10e4da67cfc49ad9bde171d
joybean <joybeanq@gmail.com>
2022-05-07 12:44:09 +0800

    Solutions for Valid Palindrome

--- src/main/java/com/joybean/leetcode/ValidPalindrome.java
+++ src/main/java/com/joybean/leetcode/ValidPalindrome.java
@@ -6,8 +6,44 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class ValidPalindrome {
-    //TODO
-    public boolean isPalindrome(String s) {
-        return false;
+    /**
+     * Two pointers
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome1(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(chars[left])) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(chars[right])) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(chars[left]) != Character.toLowerCase(chars[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/valid-palindrome/discuss/39981/My-three-line-java-solution">Use
+     * StringBuilder</a>
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome2(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(actual).reverse().toString();
+        return actual.equals(reversed);
     }
 }
