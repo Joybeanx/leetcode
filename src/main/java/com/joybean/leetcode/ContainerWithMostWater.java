aed55520011e490b7fd85e1507974dcfdd23c178
joybean <joybeanq@gmail.com>
2022-05-08 20:54:49 +0800

    Two pointers solutions for Container With Most Water

--- src/main/java/com/joybean/leetcode/ContainerWithMostWater.java
+++ src/main/java/com/joybean/leetcode/ContainerWithMostWater.java
@@ -14,13 +14,13 @@ public class ContainerWithMostWater {
      * @return
      */
     public static int maxArea1(int[] height) {
-        int maxArea = 0;
+        int ans = 0;
         for (int i = 0; i < height.length; i++) {
             for (int j = i + 1; j < height.length; j++) {
-                maxArea = Math.max(Math.min(height[i], height[j]) * (j - i), maxArea);
+                ans = Math.max(Math.min(height[i], height[j]) * (j - i), ans);
             }
         }
-        return maxArea;
+        return ans;
     }
 
     /**
@@ -38,9 +38,9 @@ public class ContainerWithMostWater {
      * @return
      */
     public static int maxArea2(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxArea = 0;
-
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
         do {
             int length = right - left;
             int minHeight;
@@ -53,29 +53,10 @@ public class ContainerWithMostWater {
                 // Find next index which value greater than current minHeight
                 right = advance(height, right, false);
             }
-            maxArea = Math.max(maxArea, minHeight * length);
+            ans = Math.max(ans, minHeight * length);
 
         } while (left < right);
-        return maxArea;
-    }
-
-    /**
-     * Two pointers: more calculation, less code
-     *
-     * @param height
-     * @return
-     */
-    public static int maxArea3(int[] height) {
-        int maxarea = 0, l = 0, r = height.length - 1;
-        while (l < r) {
-            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
-            if (height[l] < height[r]) {
-                l++;
-            } else {
-                r--;
-            }
-        }
-        return maxarea;
+        return ans;
     }
 
     private static int advance(int[] height, int start, boolean left) {
@@ -88,6 +69,74 @@ public class ContainerWithMostWater {
             }
         }
         return i;
+    }
+
+    /**
+     * Optimized two pointers
+     *
+     * @param height
+     * @return
+     */
+    public static int maxArea3(int[] height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            ans = Math.max(ans, (right - left) * h);
+            if (height[left] < height[right]) {
+                for (left = left + 1; left < right && height[left] <= h; left++) {
+                }
+            } else {
+                for (right = right - 1; left < right && height[right] <= h; right--) {
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * Two pointers: more calculation, less code
+     *
+     * @param height
+     * @return
+     */
+    public static int maxArea4(int[] height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            ans = Math.max(ans, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/container-with-most-water/">Concise two pointers</a>
+     *
+     * @param height
+     * @return
+     */
+    public static int maxArea5(int[] height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            ans = Math.max(ans, (right - left) * h);
+            while (height[left] <= h && left < right) {
+                left++;
+            }
+            while (height[right] <= h && left < right) {
+                right--;
+            }
+        }
+        return ans;
     }
 
 }
