6432a4c3266bfbe1c6f4b43f383de25ba162c51f
joybean <joybeanq@gmail.com>
2022-05-17 00:43:46 +0800

    Binary search solution for Search in Rotated Sorted Array

--- src/main/java/com/joybean/leetcode/SearchInRotatedSortedArray.java
+++ src/main/java/com/joybean/leetcode/SearchInRotatedSortedArray.java
@@ -1,13 +1,38 @@
 package com.joybean.leetcode;
 
 /**
- * <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/">Search in Rotated Sorted Array</a>
+ * <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/">Search in Rotated Sorted
+ * Array</a>
  *
  * @author Joybean
  */
 public class SearchInRotatedSortedArray {
-    //TODO
-    public int search(int[] nums, int target) {
-        return 0;
+    /**
+     * <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14435/Clever-idea-making-it-simple">
+     * Binary search</a>
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // If nums[mid] and target are "on the different side" of nums[0]
+            if (target >= nums[0] ^ nums[mid] >= nums[0]) {
+                nums[mid] = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 }
