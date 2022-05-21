0ca8a044676f2fde36280df12a1746ccd0161aa0
joybean <jianbing.sjb@alibaba-inc.com>
2022-05-21 14:22:10 +0800

    DP solution for Longest Palindromic Substring

--- src/main/java/com/joybean/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/com/joybean/leetcode/LongestPalindromicSubstring.java
@@ -6,7 +6,49 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class LongestPalindromicSubstring {
-    //TODO
+
+    /**
+     * DP
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome1(String s) {
+        int n = s.length();
+        //dp[i][j] indicates whether substring s starting at index i and ending at j is palindrome
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+        int maxLen = 1;
+        int startIdx = 0;
+        int endIdx = 0;
+        for (int j = 1; j < n; j++) {
+            dp[j][j] = true;
+            for (int i = j - 1; i >= 0; i--) {
+                if ((i == j - 1 || dp[i + 1][j - 1]) && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    if (maxLen < j - i + 1) {
+                        maxLen = j - i + 1;
+                        startIdx = i;
+                        endIdx = j;
+                    }
+                }
+
+            }
+        }
+        return s.substring(startIdx, endIdx + 1);
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using
+     * -dynamic-programming">Concise DP</a>
+     * TODO
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome2(String s) {
+        return null;
+    }
 
     /**
      * Two pointers
@@ -14,7 +56,7 @@ public class LongestPalindromicSubstring {
      * @param s
      * @return
      */
-    public static String longestPalindrome1(String s) {
+    public static String longestPalindrome3(String s) {
         return null;
     }
 
@@ -25,7 +67,7 @@ public class LongestPalindromicSubstring {
      * @param s
      * @return
      */
-    public static String longestPalindrome2(String s) {
+    public static String longestPalindrome4(String s) {
         return null;
     }
 }
