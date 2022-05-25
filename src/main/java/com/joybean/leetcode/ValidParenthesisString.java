ea1322f2692859cde5c78ced30e4b800958d84e4
joybean <joybeanq@gmail.com>
2022-05-25 17:19:56 +0800

    Greedy solution for Valid Parenthesis String

--- src/main/java/com/joybean/leetcode/ValidParenthesisString.java
+++ src/main/java/com/joybean/leetcode/ValidParenthesisString.java
@@ -77,7 +77,7 @@ public class ValidParenthesisString {
     }
 
     /**
-     * DP
+     * <a href="https://leetcode.com/problems/valid-parenthesis-string/solution/">DP</a>
      * TODO
      *
      * @param s
@@ -88,12 +88,35 @@ public class ValidParenthesisString {
     }
 
     /**
-     * Greedy
+     * <a href="https://leetcode.com/problems/valid-parenthesis-string/discuss/543521/Java-Count-Open-Parenthesis-O
+     * (n)-time-O(1)-space-Picture-Explain">Greedy</a>
      *
      * @param s
      * @return
      */
     public static boolean checkValidString4(String s) {
-        return false;
+        // open parentheses count in range [cmin, cmax]
+        int cmin = 0;
+        int cmax = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                cmax++;
+                cmin++;
+            } else if (c == ')') {
+                cmax--;
+                cmin--;
+            } else if (c == '*') {
+                cmax++; // if `*` become `(` then openCount++
+                cmin--; // if `*` become `)` then openCount--
+                // if `*` become `` then nothing happens
+                // So openCount will be in new range [cmin-1, cmax+1]
+            }
+            if (cmax < 0) {
+                return false; // Currently, don't have enough open parentheses to match close parentheses-> Invalid
+            }
+            // For example: ())(
+            cmin = Math.max(cmin, 0);   // It's invalid if open parentheses count < 0 that's why cmin can't be negative
+        }
+        return cmin == 0; // Return true if can found `openCount == 0` in range [cmin, cmax]
     }
 }
