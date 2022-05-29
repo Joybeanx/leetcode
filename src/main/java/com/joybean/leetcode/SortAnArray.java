bb72216300c771c6700be70bce479db08e2ee927
joybean <joybeanq@gmail.com>
2022-05-29 19:22:38 +0800

    Quick sort solution for Sort an Array

--- src/main/java/com/joybean/leetcode/SortAnArray.java
+++ src/main/java/com/joybean/leetcode/SortAnArray.java
@@ -1,13 +1,46 @@
 package com.joybean.leetcode;
 
 /**
- * <a href="https://leetcode.com/problems/sort-an-array/"> Sort an Array</a>
+ * <a href="https://leetcode.com/problems/sort-an-array/">Sort an Array</a>
  *
  * @author Joybean
  */
 public class SortAnArray {
-    //TODO
-    public int[] sortArray(int[] nums) {
-        return null;
+    /**
+     * Quick sort
+     *
+     * @param nums
+     * @return
+     */
+    public static int[] sortArray1(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void quickSort(int[] nums, int from, int to) {
+        if (from >= to) {
+            return;
+        }
+        int pivot = nums[from];
+        int left = from;
+        int right = to;
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            swap(nums, left, right);
+            while (left < right && nums[left] <= pivot) {
+                left++;
+            }
+            swap(nums, left, right);
+        }
+        quickSort(nums, from, left - 1);
+        quickSort(nums, left + 1, to);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
