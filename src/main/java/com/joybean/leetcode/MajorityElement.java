da01694064abc8a1300e4473ad637536ce46f29a
joybean <joybeanq@gmail.com>
2022-05-29 11:53:36 +0800

    Add Majority Element problems

---
+++ src/main/java/com/joybean/leetcode/MajorityElement.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/majority-element/">Majority Element</a>
+ *
+ * @author Joybean
+ */
+public class MajorityElement {
+    //TODO
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/joybean/leetcode/MajorityElementII.java
@@ -0,0 +1,13 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/majority-element-ii/">Majority Element II</a>
+ *
+ * @author Joybean
+ */
+public class MajorityElementII {
+    //TODO
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
