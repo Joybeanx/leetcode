410fd5f57b3713d292b65c2f3771b31c16a0f474
joybean <joybeanq@gmail.com>
2022-08-23 00:22:45 +0800

    Insertion sort solution for Sort List

--- src/main/java/com/joybean/leetcode/SortList.java
+++ src/main/java/com/joybean/leetcode/SortList.java
@@ -6,8 +6,36 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class SortList {
-    //TODO
-    public ListNode sortList(ListNode head) {
+    /**
+     * Insertion sort:Time Limit Exceeded
+     *
+     * @param head
+     * @return
+     * @see InsertionSortList#insertionSortList2(com.joybean.leetcode.InsertionSortList.ListNode)
+     */
+    public static ListNode sortList1(ListNode head) {
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            ListNode node = dummy;
+            while (node.next != null && cur.val > node.next.val) {
+                node = node.next;
+            }
+            cur.next = node.next;
+            node.next = cur;
+            cur = next;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/sort-list/solution/">Merge sort</a>
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode sortList2(ListNode head) {
         return null;
     }
 
