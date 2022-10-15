a4f5a49b816fe5180b9606eeed0769d15f63276e
joybean <joybeanq@gmail.com>
2022-10-15 23:53:33 +0800

    DP solution for Remove One Element to Make the Array Strictly Increasing

--- src/main/java/com/joybean/leetcode/RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
+++ src/main/java/com/joybean/leetcode/RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
@@ -7,8 +7,36 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RemoveOneElementToMakeTheArrayStrictlyIncreasing {
-    //TODO
-    public boolean canBeIncreasing(int[] nums) {
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param nums
+     * @return
+     */
+    public static boolean canBeIncreasing1(int[] nums) {
+        int lis = 0;
+        //dp[i] stores the length of LIS that ends with nums[i]
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            lis = Math.max(dp[i], lis);
+        }
+        return nums.length - lis <= 1;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/discuss/1299306/Two-Conditions">Straight forward</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static boolean canBeIncreasing2(int[] nums) {
         return false;
     }
 }
