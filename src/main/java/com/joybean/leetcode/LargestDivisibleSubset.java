c944337164806d087a3b1bbc4eb53ee21603f726
joybean <joybeanq@gmail.com>
2022-10-15 22:33:42 +0800

    DP solution for Largest Divisible Subset

--- src/main/java/com/joybean/leetcode/LargestDivisibleSubset.java
+++ src/main/java/com/joybean/leetcode/LargestDivisibleSubset.java
@@ -1,5 +1,7 @@
 package com.joybean.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,8 +10,42 @@ import java.util.List;
  * @author Joybean
  */
 public class LargestDivisibleSubset {
-    //TODO
-    public List<Integer> largestDivisibleSubset(int[] nums) {
+    /**
+     * Iterative(bottom-up) DP,which is concise but is not very efficient considering space
+     *
+     * @param nums
+     * @return
+     */
+    public static List<Integer> largestDivisibleSubset1(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        List[] dp = new List[nums.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = new ArrayList();
+            dp[i].add(nums[i]);
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j].size() + 1 > dp[i].size()) {
+                        dp[i] = new ArrayList(dp[j]);
+                        dp[i].add(nums[i]);
+                    }
+                }
+            }
+            if (dp[i].size() > ans.size()) {
+                ans = dp[i];
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/largest-divisible-subset/discuss/1578933/Java-or-Reuse-Longest-Increasing-Subsequence">Optimized iterative(bottom-up) DP</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static List<Integer> largestDivisibleSubset2(int[] nums) {
         return null;
     }
 }
