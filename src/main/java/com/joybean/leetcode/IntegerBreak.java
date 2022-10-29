a6c07cf4553a24d73e0dcfb29183cd26c830cf62
joybean <joybeanq@gmail.com>
2022-10-29 23:09:34 +0800

    DP solution for Integer Break

--- src/main/java/com/joybean/leetcode/IntegerBreak.java
+++ src/main/java/com/joybean/leetcode/IntegerBreak.java
@@ -6,8 +6,41 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class IntegerBreak {
-    //TODO
-    public int integerBreak(int n) {
-        return 0;
+    /**
+     * <a href="https://leetcode.com/problems/integer-break/discuss/80694/Java-DP-solution">Iterative(bottom-up) DP (by kvmial)</a>
+     *
+     * @param n
+     * @return
+     */
+    public static int integerBreak1(int n) {
+        //dp[i] represents the maximum product of i
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]), dp[i]);
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/integer-break/discuss/80694/Java-DP-solution">Optimized iterative(bottom-up) DP: init dp array first (by cdai)</a>
+     *
+     * @param n
+     * @return
+     */
+    public static int integerBreak2(int n) {
+        //dp[i] represents the maximum product of i
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < n; i++) {
+            dp[i] = i;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i - j; j++) {
+                dp[i] = Math.max(dp[j] * dp[i - j], dp[i]);
+            }
+        }
+        return dp[n];
     }
 }
