0003e91aa1f1796656eb47046fe1f9d6d1f89917
joybean <joybeanq@gmail.com>
2023-08-09 17:19:39 +0800

    Lower bound solution for Binary Search

--- src/main/java/com/joybean/leetcode/BinarySearch.java
+++ src/main/java/com/joybean/leetcode/BinarySearch.java
@@ -52,10 +52,37 @@ public class BinarySearch {
             if (nums[mid] < target) {
                 left = mid + 1;
             } else {
-                //much better than: right = mid;
                 right = mid - 1;
             }
         }
         return nums[left] == target ? left : -1;
+    }
+
+    /**
+     * <a href="https://www.zhihu.com/question/36132386">Binary search 3:find lower bound</a>
+     *
+     * @param nums
+     * @param target
+     * @return
+     * @see <a href ="https://github.com/python/cpython/blob/3.9/Lib/bisect.py#L50">Python bisect_left</a>
+     */
+    public static int search3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        //search range [left,right)
+        while (left < right) {
+            //avoid overflow
+            int mid = (left + right) >>> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        //Integer in [0,left) must be less than target
+        if (left != nums.length && nums[left] == target) {
+            return left;
+        }
+        return -1;
     }
 }
