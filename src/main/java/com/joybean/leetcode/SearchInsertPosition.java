40685cf761e00e194d593409dc585b22d5d530b0
joybean <joybeanq@gmail.com>
2023-08-10 13:49:06 +0800

    Polish

--- src/main/java/com/joybean/leetcode/SearchInsertPosition.java
+++ src/main/java/com/joybean/leetcode/SearchInsertPosition.java
@@ -16,7 +16,7 @@ public class SearchInsertPosition {
     public static int searchInsert1(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        //search range [left,right]
+        //search range [0,nums.length - 1]
         while (left <= right) {
             //avoid overflow
             int mid = (left + right) >>> 1;
@@ -33,7 +33,7 @@ public class SearchInsertPosition {
     }
 
     /**
-     * Binary search 2
+     * <a href="https://leetcode.com/problems/search-insert-position/discuss/249092/Come-on-forget-the-binary-search-patterntemplate!-Try-understand-it!">Binary search 2</a>
      *
      * @param nums
      * @param target
@@ -41,8 +41,40 @@ public class SearchInsertPosition {
      */
     public static int searchInsert2(int[] nums, int target) {
         int left = 0;
+        int right = nums.length - 1;
+        //search range [0,nums.length - 1)
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                //mid may be a possible insert position
+                right = mid;
+            }
+        }
+        //handle the case that left stops at n-1 and desired insert position is n
+        //nums=[1,3] target=5
+        if (left == nums.length - 1 && nums[left] < target) {
+            return left + 1;
+        }
+        return left;
+    }
+
+
+    /**
+     * Binary search 3
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert3(int[] nums, int target) {
+        int left = 0;
         int right = nums.length;
-        //search range [left,right)
+        //search range [0,nums.length)
         while (left < right) {
             //Always gives the lower mid,so that this won't make the search range stuck
             int mid = (left + right - 1) >>> 1;
@@ -60,35 +92,6 @@ public class SearchInsertPosition {
         return left;
     }
 
-    /**
-     * <a href="https://leetcode.com/problems/search-insert-position/discuss/249092/Come-on-forget-the-binary-search-patterntemplate!-Try-understand-it!">Binary search 3</a>
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public static int searchInsert3(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) >>> 1;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                //mid may be a possible insert position
-                right = mid;
-            }
-        }
-        //handle the case that left stops at n-1 and desired insert position is n
-        //nums=[1,3] target=5
-        if (left == nums.length - 1 && nums[left] < target) {
-            return left + 1;
-        }
-        return left;
-    }
-
 
     /**
      * <a href="https://leetcode.com/problems/search-insert-position/discuss/423166/Binary-Search-101">Binary search 4:find lower bound</a>
@@ -101,7 +104,7 @@ public class SearchInsertPosition {
     public static int searchInsert4(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
-        //search range [left,right)
+        //search range [0,nums.length)
         while (left < right) {
             int mid = (left + right) >>> 1;
             //100% sure logic
