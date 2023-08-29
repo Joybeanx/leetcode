8c34d268994e1574a1830843cb51a1dce3110d0d
joybeanx <joybeanq@gmail.com>
2023-08-29 08:28:57 +0800

    Sliding window solution for Shortest Subarray to be Removed to Make Array Sorted

--- src/main/java/com/joybean/leetcode/ShortestSubarrayToBeRemovedToMakeArraySorted.java
+++ src/main/java/com/joybean/leetcode/ShortestSubarrayToBeRemovedToMakeArraySorted.java
@@ -8,19 +8,41 @@ package com.joybean.leetcode;
  */
 public class ShortestSubarrayToBeRemovedToMakeArraySorted {
     /**
-     * Sliding window
-     * //TODO
+     * <a href="https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/solutions/830480/c-o-n-sliding-window-explanation-with-illustrations/">Sliding window</a>
      *
      * @param arr
      * @return
      */
     public static int findLengthOfShortestSubarray1(int[] arr) {
-        return 0;
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left++;
+        }
+        if (left == n - 1) {
+            return 0;
+        }
+        while (right - 1 >= 0 && arr[right] >= arr[right - 1]) {
+            right--;
+        }
+        int ans = Math.min(n - left - 1, right);
+        int i = 0;
+        int j = right;
+        while (i <= left && j < n) {
+            if (arr[i] <= arr[j]) {
+                ans = Math.min(j - i - 1, ans);
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
     }
 
     /**
      * Two pointers
-     *
+     * <p>
      * TODO
      *
      * @param arr
