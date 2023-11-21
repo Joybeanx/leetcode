54eb75ed2ec252941c29a8236e636ff3bf46e9dd
joybean <joybeanq@gmail.com>
2023-11-21 11:15:32 +0800

    Two pointers solution for Trapping Rain Water

--- src/main/java/com/joybean/leetcode/TrappingRainWater.java
+++ src/main/java/com/joybean/leetcode/TrappingRainWater.java
@@ -38,13 +38,34 @@ public class TrappingRainWater {
 
     /**
      * <a href="https://leetcode.com/problems/trapping-rain-water/solution/">Using two pointers</a>
-     * TODO
      *
      * @param height
      * @return
      */
     public static int trap2(int[] height) {
-        return 0;
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left <= right) {
+            if (leftMax < rightMax) {
+                if (height[left] < leftMax) {
+                    ans += leftMax - height[left];
+                } else {
+                    leftMax = height[left];
+                }
+                left++;
+            } else {
+                if (height[right] < rightMax) {
+                    ans += rightMax - height[right];
+                } else {
+                    rightMax = height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
     }
 
     /**
