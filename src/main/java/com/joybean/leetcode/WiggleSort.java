f6582c652ceaa4317039f5daba4261bcf4b38c3b
joybean <joybeanq@gmail.com>
2023-12-18 14:27:07 +0800

    Solution for Wiggle Sort

--- src/main/java/com/joybean/leetcode/WiggleSort.java
+++ src/main/java/com/joybean/leetcode/WiggleSort.java
@@ -6,14 +6,32 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class WiggleSort {
-    //TODO
 
     /**
+     * Swap
      * https://www.lintcode.com/problem/508/
      *
      * @return
      */
-    public void wiggleSort(int[] nums) {
-        // write your code here
+    public static void wiggleSort(int[] nums) {
+        //If i is even and nums[i] > nums[i+1], then swap the elements
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((i & 1) == 0) {
+                if (nums[i] > nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+                //If i is odd and nums[i] < nums[i+1], then swap the elements
+            } else {
+                if (nums[i] < nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
