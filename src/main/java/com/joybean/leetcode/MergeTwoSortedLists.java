df4f1647769938af84e8c7000a5294448fb6c5dc
joybeanx <joybeanq@gmail.com>
2024-01-20 11:13:07 +0800

    Polish

--- src/main/java/com/joybean/leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/joybean/leetcode/MergeTwoSortedLists.java
@@ -14,22 +14,22 @@ public class MergeTwoSortedLists {
      * @return
      */
     public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
-        ListNode start = new ListNode(0);
+        ListNode dummy = new ListNode(0);
         ListNode p1 = l1;
         ListNode p2 = l2;
-        ListNode cur = start;
+        ListNode tail = dummy;
         while (p1 != null && p2 != null) {
             if (p1.val < p2.val) {
-                cur.next = p1;
+                tail.next = p1;
                 p1 = p1.next;
             } else {
-                cur.next = p2;
+                tail.next = p2;
                 p2 = p2.next;
             }
-            cur = cur.next;
+            tail = tail.next;
         }
-        cur.next = p1 != null ? p1 : p2;
-        return start.next;
+        tail.next = p1 != null ? p1 : p2;
+        return dummy.next;
     }
 
     /**
