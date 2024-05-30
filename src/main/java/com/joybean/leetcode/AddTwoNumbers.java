d4148da08f334fb8327da2463c36730261238392
joybeanx <joybeanq@gmail.com>
2024-05-30 08:58:43 +0800

    New solution for Add Two Numbers

--- src/main/java/com/joybean/leetcode/AddTwoNumbers.java
+++ src/main/java/com/joybean/leetcode/AddTwoNumbers.java
@@ -57,23 +57,66 @@ public class AddTwoNumbers {
     }
 
     public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
         int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
             carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+            int val = sum % 10;
+            cur.next = new ListNode(val);
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+            int sum = l1.val + carry;
+            int val = sum % 10;
+            carry = sum / 10;
+            cur.next = new ListNode(val);
+            l1 = l1.next;
+            cur = cur.next;
+        }
+        while (l2 != null) {
+            int sum = l2.val + carry;
+            int val = sum % 10;
+            carry = sum / 10;
+            cur.next = new ListNode(val);
+            l2 = l2.next;
+            cur = cur.next;
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+
+    /**
+     * Concise solution
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            int sum = carry + val1 + val2;
+            carry = sum / 10;
+            int val = sum % 10;
+            cur.next = new ListNode(val);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
         if (carry > 0) {
-            curr.next = new ListNode(carry);
+            cur.next = new ListNode(carry);
         }
-        return dummyHead.next;
+        return dummy.next;
     }
 
     static class ListNode {
