65e57c7f45441ba4ad56849a44a077e09e2ef562
joybean <joybeanq@gmail.com>
2024-06-06 18:05:22 +0800

    Remove uncessary variables

--- src/main/java/com/joybean/leetcode/CoinChange.java
+++ src/main/java/com/joybean/leetcode/CoinChange.java
@@ -44,7 +44,6 @@ public class CoinChange {
         int[] dp = new int[amount + 1];
         //initialize array with amount+1
         Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
                 if (i < coin) {
@@ -70,7 +69,6 @@ public class CoinChange {
         int[] dp = new int[amount + 1];
         //initialize array with amount+1
         Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
         //put coins at outer loop to avoid some dp slots
         for (int coin : coins) {
             for (int i = coin; i <= amount; i++) {
