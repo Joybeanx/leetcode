2759fae3404c06460e094f3b08e742998d172c7f
joybeanx <joybeanq@gmail.com>
2024-06-09 16:23:17 +0800

    New Iterative(bottom-up) DP for Minimum Path Sum

--- src/main/java/com/joybean/leetcode/MinimumPathSum.java
+++ src/main/java/com/joybean/leetcode/MinimumPathSum.java
@@ -1,11 +1,14 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/minimum-path-sum/">Minimum Path Sum</a>
  *
  * @author Joybean
  */
 public class MinimumPathSum {
+
     /**
      * Iterative(bottom-up) DP
      *
@@ -13,6 +16,32 @@ public class MinimumPathSum {
      * @return
      */
     public static int minPathSum1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        Arrays.fill(dp[0], Integer.MAX_VALUE);
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = Integer.MAX_VALUE;
+        }
+        dp[1][1] = grid[0][0];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i != 1 || j != 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param grid
+     * @return
+     */
+    public static int minPathSum3(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
@@ -29,6 +58,34 @@ public class MinimumPathSum {
         return dp[m - 1][n - 1];
     }
 
+
+    /**
+     * <a href="https://leetcode.com/problems/minimum-path-sum/">Iterative(bottom-up) DP: initialize dp[0][j] and dp[i][0]</a>
+     *
+     * @param grid
+     * @return
+     */
+    public static int minPathSum4(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+
     /**
      * Recursive(top-down) DP
      * TODO
@@ -36,7 +93,9 @@ public class MinimumPathSum {
      * @param grid
      * @return
      */
-    public static int minPathSum2(int[][] grid) {
+    public static int minPathSum5(int[][] grid) {
         return 0;
     }
+
+
 }
