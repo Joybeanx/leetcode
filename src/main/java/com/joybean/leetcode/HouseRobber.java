099e4582252a59dfbbf6a1577695972d642307d4
joybeanx <joybeanq@gmail.com>
2024-06-16 19:53:57 +0800

    New DP solution for House Robber

--- src/main/java/com/joybean/leetcode/HouseRobber.java
+++ src/main/java/com/joybean/leetcode/HouseRobber.java
@@ -21,21 +21,37 @@ public class HouseRobber {
     }
 
     /**
-     * Iterative(bottom-up) DP with state compression
+     * Iterative(bottom-up) DP
      *
      * @param nums
      * @return
      */
     public static int rob2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        dp[1] = nums[0];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[n];
+    }
+
+    /**
+     * Iterative(bottom-up) DP with state compression
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob3(int[] nums) {
         int cur, prev1, prev2;
         cur = prev1 = prev2 = 0;
-        for (
-            int i = nums.length - 1;
-            i >= 0; i--) {
+        for (int i = nums.length - 1; i >= 0; i--) {
             cur = Math.max(nums[i] + prev2, prev1);
             prev2 = prev1;
             prev1 = cur;
         }
         return cur;
     }
+
+
 }
