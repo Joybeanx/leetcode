9c12c73b6a4a45d4bfa2d290e628881bd3979b2d
joybeanx <joybeanq@gmail.com>
2024-06-18 14:39:13 +0800

    New monotonic stack solution for Remove K Digits

--- src/main/java/com/joybean/leetcode/RemoveKDigits.java
+++ src/main/java/com/joybean/leetcode/RemoveKDigits.java
@@ -1,7 +1,6 @@
 package com.joybean.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 /**
  * <a href="https://leetcode.com/problems/remove-k-digits/">Remove K Digits</a>
@@ -17,32 +16,69 @@ public class RemoveKDigits {
      * @return
      */
     public static String removeKdigits1(String num, int k) {
-        Deque<Character> stack = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
-        for (char ch : num.toCharArray()) {
-            while (!stack.isEmpty() && stack.peek() > ch && k > 0) {
+        int n = num.length();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            Character c = num.charAt(i);
+            while (!stack.isEmpty() && stack.peek() > c && k > 0) {
                 stack.pop();
                 k--;
             }
-            stack.push(ch);
+            stack.push(c);
+        }
+        //case like "1111" or "1234"
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        //trim leading zeros
+        int i = 0;
+        while (i < stack.size() && stack.get(i) == '0') {
+            i++;
+        }
+        String ans = "";
+        while (i < stack.size()) {
+            ans += stack.get(i++);
+        }
+        return ans == "" ? "0" : ans;
+    }
+
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-k-digits/solutions/629860/java-c-python3-easy-explanation/">Monotonic stack</a>
+     *
+     * @param num
+     * @param k
+     * @return
+     */
+    public static String removeKdigits2(String num, int k) {
+        int n = num.length();
+        if (k == n) {
+            return "0";
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            Character c = num.charAt(i);
+            while (!stack.isEmpty() && stack.peek() > c && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
         }
         //case like "1111" or "1234"
         while (k-- > 0) {
             stack.pop();
         }
-
-        //trim leading zeros
-        while (!stack.isEmpty() && stack.peekLast() == '0') {
-            stack.pollLast();
-        }
-
+        String ans = "";
         while (!stack.isEmpty()) {
-            sb.append(stack.pollLast());
+            ans = stack.pop() + ans;
         }
-        String ans = sb.toString();
-        if (ans.isEmpty()) {
-            return "0";
+        int i = 0;
+        //trim leading zeros
+        while (i < ans.length() - 1 && ans.charAt(i) == '0') {
+            i++;
         }
-        return ans;
+        return ans.substring(i);
     }
+
 }
