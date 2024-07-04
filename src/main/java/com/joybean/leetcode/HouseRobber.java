657405e0ec974479ed38959545f0ee0c0677b365
joybean <joybeanq@gmail.com>
2024-07-04 14:57:49 +0800

    New DP solutions for House Robber

--- src/main/java/com/joybean/leetcode/HouseRobber.java
+++ src/main/java/com/joybean/leetcode/HouseRobber.java
@@ -1,5 +1,7 @@
 package com.joybean.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/house-robber/">House Robber</a>
  *
@@ -28,6 +30,7 @@ public class HouseRobber {
      */
     public static int rob2(int[] nums) {
         int n = nums.length;
+        //dp[i] represents maximum amount of money can rob from nums[0...i-1]
         int[] dp = new int[n + 1];
         dp[1] = nums[0];
         for (int i = 2; i <= n; i++) {
@@ -37,21 +40,68 @@ public class HouseRobber {
     }
 
     /**
-     * Iterative(bottom-up) DP with state compression
+     * Iterative(bottom-up) DP (Recommended)
      *
      * @param nums
      * @return
      */
     public static int rob3(int[] nums) {
-        int cur, prev1, prev2;
-        cur = prev1 = prev2 = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            cur = Math.max(nums[i] + prev2, prev1);
-            prev2 = prev1;
-            prev1 = cur;
+        int n = nums.length;
+        //dp[i] represents maximum amount of money can rob from nums[0...i-2]
+        int[] dp = new int[n + 2];
+        for (int i = 2; i < n + 2; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1]);
         }
-        return cur;
+        return dp[n + 1];
     }
 
+    /**
+     * Iterative(bottom-up) DP with state compression
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob4(int[] nums) {
+        int n = nums.length;
+        int prev1 = 0;
+        int prev2 = 0;
+        int ans = 0;
+        for (int i = 2; i < n + 2; i++) {
+            int tmp = ans;
+            ans = Math.max(prev2 + nums[i - 2], prev1);
+            prev1 = ans;
+            prev2 = tmp;
+        }
+        return ans;
+    }
+
+    /**
+     * Recursive(top-down) DP with memo
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob5(int[] nums) {
+        int n = nums.length;
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return rob5(n - 1, nums, memo);
+    }
+
+    private static int rob5(int toIdx, int[] nums, int[] memo) {
+        if (toIdx < 0) {
+            return 0;
+        }
+        //unnecessary exit point
+        //if (toIdx == 0) {
+        //    return nums[toIdx];
+        //}
+        if (memo[toIdx] != -1) {
+            return memo[toIdx];
+        }
+        int ans = Math.max(rob5(toIdx - 1, nums, memo), nums[toIdx] + rob5(toIdx - 2, nums, memo));
+        memo[toIdx] = ans;
+        return ans;
+    }
 
 }
