2631d4c71404fb709b8c3d4e014f37035f550a91
joybean <joybeanq@gmail.com>
2024-07-05 00:02:02 +0800

    Polish

--- src/main/java/com/joybean/leetcode/TargetSum.java
+++ src/main/java/com/joybean/leetcode/TargetSum.java
@@ -37,7 +37,7 @@ public class TargetSum {
         int dp[][] = new int[n + 1][target + 1];
         dp[0][0] = 1;
         for (int i = 1; i <= n; i++) {
-            //j must start from 0 instead of nums[i-1], case: nums = [0,0,0,0,0,0,0,0,1], target=1
+            //j must start from 0 instead of nums[i-1], case: nums = [0,1], target=1
             for (int j = 0; j <= target; j++) {
                 dp[i][j] = dp[i - 1][j];
                 if (j >= nums[i - 1]) {
