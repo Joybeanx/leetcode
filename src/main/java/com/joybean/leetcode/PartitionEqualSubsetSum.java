222f687417ae1d5d8f97c94d1c5fbfe8d85b5327
joybeanx <joybeanq@gmail.com>
2024-07-08 00:46:06 +0800

    New DP solution for Partition Equal Subset Sum

--- src/main/java/com/joybean/leetcode/PartitionEqualSubsetSum.java
+++ src/main/java/com/joybean/leetcode/PartitionEqualSubsetSum.java
@@ -9,55 +9,69 @@ import java.util.Arrays;
  */
 public class PartitionEqualSubsetSum {
     /**
-     * DP
+     * Iterative(bottom-up) DP
      *
      * @param nums
      * @return
      */
     public static boolean canPartition1(int[] nums) {
         int sum = Arrays.stream(nums).sum();
-        if (sum % 2 != 0) {
+        if ((sum & 1) == 1) {
             return false;
         }
+        int n = nums.length;
         int target = sum / 2;
-        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
-
-        for (int i = 0; i < nums.length; i++) {
-            dp[i][0] = true;
-        }
-        for (int j = 0; j <= sum; j++) {
-            dp[0][j] = false;
-        }
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (j < nums[i - 1]) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-                }
+        //dp[i][j] indicates whether it is possible to pick numbers from nums[0..i-1] so that their sum is equal to j
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            //should start from 1 rather than nums[i-1]
+            for (int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j] || (j - nums[i - 1] >= 0 && dp[i - 1][j - nums[i - 1]]);
             }
         }
         return dp[nums.length][target];
     }
 
+
+    /*
+    //wrong solution, previous updates on dp[j] will affect later dp[j - nums[i - 1]] calculation in the same row
+    //case: nums=[1,2,5]
+    public static boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        int target = sum / 2;
+        int n = nums.length;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                dp[j] = dp[j] || (j - nums[i - 1] >= 0 && dp[j - nums[i - 1]]);
+            }
+        }
+        return dp[target];
+    }
+    */
     /**
      * Iterative(bottom-up) DP with state compression
+     *
      * @param nums
      * @return
      */
     public static boolean canPartition2(int[] nums) {
         int sum = Arrays.stream(nums).sum();
-        if (sum % 2 != 0) {
+        if ((sum & 1) == 1) {
             return false;
         }
         int target = sum / 2;
-        boolean[] dp = new boolean[sum + 1];
+        int n = nums.length;
+        boolean[] dp = new boolean[target + 1];
         dp[0] = true;
-        for (int i = 1; i <= nums.length; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = target; j >= 1; j--) {
-                if (j >= nums[i - 1]) {
-                    dp[j] = dp[j - nums[i - 1]] || dp[j];
-                }
+                dp[j] = dp[j] || (j - nums[i - 1] >= 0 && dp[j - nums[i - 1]]);
             }
         }
         return dp[target];
