55c83d1ea1e427cdca5e00ac8296368a636aa0b3
joybean <joybeanq@gmail.com>
2024-07-08 14:25:08 +0800

    Greedy algorithm solution for Queue Reconstruction by Height

--- src/main/java/com/joybean/leetcode/QueueReconstructionByHeight.java
+++ src/main/java/com/joybean/leetcode/QueueReconstructionByHeight.java
@@ -2,6 +2,8 @@ package com.joybean.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,6 +12,11 @@ import java.util.List;
  * @author Joybean
  */
 public class QueueReconstructionByHeight {
+    /**
+     * Greedy algorithm using ArrayList
+     * @param people
+     * @return
+     */
     public static int[][] reconstructQueue1(int[][] people) {
         //order by height desc, position asc
         Arrays.sort(people, (p1, p2) -> p1[0] != p2[0] ? p2[0] - p1[0] : p1[1] - p2[1]);
@@ -18,5 +25,27 @@ public class QueueReconstructionByHeight {
             ans.add(person[1], person);
         }
         return ans.toArray(new int[ans.size()][]);
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/queue-reconstruction-by-height/solutions/2211641/visual-explanation
+     * -java-greedy/">Greedy algorithm using LinkedList</a>
+     *
+     * @param people
+     * @return
+     */
+    public int[][] reconstructQueue(int[][] people) {
+        int n = people.length;
+        List<int[]> list = new LinkedList<>();
+        Arrays.sort(people, (a, b) -> {
+            if (b[0] == a[0]) {
+                return a[1] - b[1];
+            }
+            return b[0] - a[0];
+        });
+        for (int i = 0; i < n; i++) {
+            list.add(people[i][1], people[i]);
+        }
+        return list.toArray(new int[n][2]);
     }
 }
