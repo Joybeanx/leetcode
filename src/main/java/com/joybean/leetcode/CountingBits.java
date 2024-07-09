03039bb79fc2796f6a7281cd2d7d54a6afb6ff53
joybean <joybeanq@gmail.com>
2024-07-09 11:31:43 +0800

    Brute force solution for Counting Bits

--- src/main/java/com/joybean/leetcode/CountingBits.java
+++ src/main/java/com/joybean/leetcode/CountingBits.java
@@ -7,12 +7,34 @@ package com.joybean.leetcode;
  */
 public class CountingBits {
     /**
-     * Iterative(bottom-up) DP
+     * Brute force
      *
      * @param n
      * @return
      */
     public static int[] countBits1(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int num = i;
+            int cnt = 0;
+            while (num != 0) {
+                if ((num & 1) == 1) {
+                    cnt++;
+                }
+                num = num >> 1;
+            }
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param n
+     * @return
+     */
+    public static int[] countBits2(int n) {
         if (n == 0) {
             return new int[1];
         }
@@ -22,7 +44,7 @@ public class CountingBits {
             //for even numbers, number of ones will be always the same as their halves
             if ((i & 1) == 0) {
                 dp[i] = dp[half];
-            //for old numbers, number of ones will be number of ones in their halves + 1
+                //for old numbers, number of ones will be number of ones in their halves + 1
             } else {
                 dp[i] = dp[half] + 1;
             }
@@ -36,7 +58,7 @@ public class CountingBits {
      * @param n
      * @return
      */
-    public static int[] countBits2(int n) {
+    public static int[] countBits3(int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             dp[i] = dp[i >> 1] + (i & 1);
