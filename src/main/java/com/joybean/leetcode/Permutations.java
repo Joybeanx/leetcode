5f983079afd360f14fb2fadcb12c3009bd2637ea
joybeanx <joybeanq@gmail.com>
2024-07-16 00:11:58 +0800

    Add todo

--- src/main/java/com/joybean/leetcode/Permutations.java
+++ src/main/java/com/joybean/leetcode/Permutations.java
@@ -35,4 +35,26 @@ public class Permutations {
             curPath.remove(curPath.size() - 1);
         }
     }
+
+    /**
+     * <a href="https://leetcode.cn/problems/permutations/solutions/2363882/46-quan-pai-lie-hui-su-qing-xi-tu-jie-by-6o7h/?envType=problem-list-v2&envId=2cktkvj">Optimized backtracking using swap</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute2(int[] nums) {
+        return null;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/permutations/solutions/18255/share-my-short-iterative-java-solution/">Iterative solution</a>
+     * TODO
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute3(int[] nums) {
+        return null;
+    }
 }
