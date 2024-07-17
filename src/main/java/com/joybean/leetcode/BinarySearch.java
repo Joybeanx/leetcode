7ad2e9d61dd143bdab1b83df6e5c7402ca17fe66
joybean <joybeanq@gmail.com>
2024-07-17 15:59:52 +0800

    New binary search solution for Binary Search

--- src/main/java/com/joybean/leetcode/BinarySearch.java
+++ src/main/java/com/joybean/leetcode/BinarySearch.java
@@ -92,9 +92,12 @@ public class BinarySearch {
         return -1;
     }
 
+
+
     /**
      *
-     * <a href="https://www.zhihu.com/question/36132386">Binary search 4:find lower bound, search range [0,nums.length - 1)</a>
+     *
+     * <a href="https://www.zhihu.com/question/36132386">Binary search 4:find lower bound, search range [0,nums.length-1)</a>
      *
      * @param nums
      * @param target
@@ -117,5 +120,36 @@ public class BinarySearch {
         //while loop only exits when left == right, we should determine whether the last element is our target
         //case:nums=[5],target=5
         return nums[left] == target ? left : -1;
+    }
+
+    /**
+     * <a href="https://www.zhihu.com/question/36132386">Binary search 5:find lower bound, search range
+     * [0,nums.length)</a>
+     *
+     * @param nums
+     * @param target
+     * @return
+     * @see <a href ="https://github.com/python/cpython/blob/3.9/Lib/bisect.py#L50">Python bisect_left</a>
+     */
+    public static int search5(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        //search range [0,nums.length)
+        //loop invariants:
+        //for each num in [0, left), num < target
+        //[left, right) is not empty, left < right
+        //for each num in [right, length), num >= target
+        while (left < right) {
+            //avoid overflow
+            int mid = (left + right) >>> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
