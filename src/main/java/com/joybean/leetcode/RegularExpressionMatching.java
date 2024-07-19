4b8f36f8a50ab5ae6f6b74749b08cd854c8134c3
joybeanx <joybeanq@gmail.com>
2024-07-19 23:47:50 +0800

    Add todo

--- src/main/java/com/joybean/leetcode/RegularExpressionMatching.java
+++ src/main/java/com/joybean/leetcode/RegularExpressionMatching.java
@@ -6,18 +6,37 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class RegularExpressionMatching {
-    public boolean isMatch(String s, String p) {
+    /**
+     * <a href="https://leetcode.com/problems/regular-expression-matching/editorial/">Recursion</a>
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public static boolean isMatch1(String s, String p) {
         if (p.isEmpty()) {
             return s.isEmpty();
         }
-        boolean first_match = (!s.isEmpty() &&
+        boolean firstMatch = (!s.isEmpty() &&
                 (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
 
         if (p.length() >= 2 && p.charAt(1) == '*') {
-            return (isMatch(s, p.substring(2)) ||
-                    (first_match && isMatch(s.substring(1), p)));
+            return (isMatch1(s, p.substring(2)) ||
+                    (firstMatch && isMatch1(s.substring(1), p)));
         } else {
-            return first_match && isMatch(s.substring(1), p.substring(1));
+            return firstMatch && isMatch1(s.substring(1), p.substring(1));
         }
+    }
+
+    /**
+     * Iterative(bottom-up) DP
+     * TODO
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public static boolean isMatch2(String s, String p) {
+        return false;
     }
 }
