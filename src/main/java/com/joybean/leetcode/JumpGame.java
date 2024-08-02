1f669364f72447b4327c0fd25cae4ed04a167f05
joybeanx <joybeanq@gmail.com>
2024-08-02 14:14:31 +0800

    Polish

--- src/main/java/com/joybean/leetcode/JumpGame.java
+++ src/main/java/com/joybean/leetcode/JumpGame.java
@@ -103,16 +103,4 @@ public class JumpGame {
         }
         return true;
     }
-
-
-    public boolean canJump(int[] nums) {
-        int maxReachable = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > maxReachable) {
-                return false;
-            }
-            maxReachable = Math.max(i + nums[i], maxReachable);
-        }
-        return true;
-    }
 }
