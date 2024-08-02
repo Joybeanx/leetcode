deb1694e47ccd11ec72b72120cc630e25356ea34
joybeanx <joybeanq@gmail.com>
2024-08-02 16:39:11 +0800

    New backtracking solution for Permutations

--- src/main/java/com/joybean/leetcode/Permutations.java
+++ src/main/java/com/joybean/leetcode/Permutations.java
@@ -1,7 +1,7 @@
 package com.joybean.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * <a href="https://leetcode.com/problems/permutations/">Permutations</a>
@@ -10,18 +10,18 @@ import java.util.List;
  */
 public class Permutations {
     /**
-     * backtracking
+     * backtracking using LinkedHashSet
      *
      * @param nums
      * @return
      */
     public static List<List<Integer>> permute1(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(ans, nums, new ArrayList<>());
+        backtrack1(new LinkedHashSet<>(), nums, ans);
         return ans;
     }
 
-    private static void backtrack(List<List<Integer>> ans, int[] nums, List<Integer> curPath) {
+    private static void backtrack1(LinkedHashSet<Integer> curPath, int[] nums, List<List<Integer>> ans) {
         if (curPath.size() == nums.length) {
             ans.add(new ArrayList<>(curPath));
             return;
@@ -31,20 +31,74 @@ public class Permutations {
                 continue;
             }
             curPath.add(num);
-            backtrack(ans, nums, curPath);
+            backtrack1(curPath, nums, ans);
             curPath.remove(curPath.size() - 1);
         }
     }
 
     /**
-     * <a href="https://leetcode.cn/problems/permutations/solutions/2363882/46-quan-pai-lie-hui-su-qing-xi-tu-jie-by-6o7h/?envType=problem-list-v2&envId=2cktkvj">Optimized backtracking using swap</a>
-     * TODO
+     * backtracking based on swap
      *
      * @param nums
      * @return
      */
     public static List<List<Integer>> permute2(int[] nums) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack2(new LinkedList<>(), 0, nums, ans);
+        return ans;
+    }
+
+    private static void backtrack2(LinkedList<Integer> curPath, int startIdx, int[] nums, List<List<Integer>> ans) {
+        if (curPath.size() == nums.length) {
+            ans.add(new ArrayList<>(curPath));
+            return;
+        }
+        for (int i = startIdx; i < nums.length; i++) {
+            swap(startIdx, i, nums);
+            curPath.add(nums[startIdx]);
+            backtrack2(curPath, startIdx + 1, nums, ans);
+            curPath.removeLast();
+            swap(startIdx, i, nums);
+        }
+    }
+
+    private static void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    /**
+     * <a href="https://leetcode.cn/problems/permutations/?envType=problem-list-v2&envId=2cktkvj">Optimized backtracking based on swap</a>
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute3(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        backtrack3(0, numList, ans);
+        return ans;
+    }
+
+    private static void backtrack3(int startIdx, List<Integer> nums,
+                                   List<List<Integer>> ans) {
+        if (startIdx == nums.size()) {
+            ans.add(new ArrayList<>(nums));
+            return;
+        }
+        for (int i = startIdx; i < nums.size(); i++) {
+            swap(startIdx, i, nums);
+            backtrack3(startIdx + 1, nums, ans);
+            swap(startIdx, i, nums);
+        }
+    }
+
+    private static void swap(int i, int j, List<Integer> nums) {
+        int tmp = nums.get(i);
+        nums.set(i, nums.get(j));
+        nums.set(j, tmp);
     }
 
     /**
@@ -54,7 +108,9 @@ public class Permutations {
      * @param nums
      * @return
      */
-    public static List<List<Integer>> permute3(int[] nums) {
+    public static List<List<Integer>> permute4(int[] nums) {
         return null;
     }
+
+
 }
