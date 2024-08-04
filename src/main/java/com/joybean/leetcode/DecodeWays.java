4322508ce6585b245f083bcdd2fe709d13fc3d84
joybeanx <joybeanq@gmail.com>
2024-08-05 00:38:34 +0800

    New DP solution for Decode Ways

--- src/main/java/com/joybean/leetcode/DecodeWays.java
+++ src/main/java/com/joybean/leetcode/DecodeWays.java
@@ -13,6 +13,31 @@ public class DecodeWays {
      * @return
      */
     public static int numDecodings1(String s) {
+        if (s.startsWith("0")) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if (s.charAt(i - 2) != '0' && Integer.valueOf(s.substring(i - 2, i)) <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * Iterative(bottom-up) DP
+     *
+     * @param s
+     * @return
+     */
+    public static int numDecodings2(String s) {
         //dp[i] represents decoding ways of s[0,i-1]
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
@@ -31,4 +56,17 @@ public class DecodeWays {
         }
         return dp[s.length()];
     }
+
+    /**
+     * Recursive(top-down) DP with memo
+     * TODO
+     *
+     * @param s
+     * @return
+     */
+    public static int numDecodings3(String s) {
+        return 0;
+    }
+
+
 }
