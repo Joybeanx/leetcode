b1a9776c6febe2243dabf7e50f51cddef0288b60
joybeanx <joybeanq@gmail.com>
2024-08-06 23:57:40 +0800

    New solution for Basic Calculator

--- src/main/java/com/joybean/leetcode/BasicCalculator.java
+++ src/main/java/com/joybean/leetcode/BasicCalculator.java
@@ -8,13 +8,76 @@ import java.util.Stack;
  * @author Joybean
  */
 public class BasicCalculator {
+
     /**
-     * <a href="https://leetcode.com/problems/basic-calculator/solutions/62361/iterative-java-solution-with-stack/">Iterative solution with stack</a>
+     * Straight forward: Time Limit Exceeded
      *
      * @param s
      * @return
      */
-    public static int calculate(String s) {
+    public static int calculate1(String s) {
+        Stack<String> stack = new Stack<>();
+        int ans = 0;
+        Integer num = 0;
+        for (int i = 0; i < s.toCharArray().length; i++) {
+            Character c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+            } else if (c == '+' || c == '-') {
+                stack.push(num.toString());
+                stack.push(c.toString());
+                num = 0;
+
+            } else if (c == '(') {
+                stack.push(c.toString());
+
+            } else if (c == ')') {
+                stack.push(num.toString());
+                num = 0;
+                Integer n = 0;
+                int k = 0;
+                while (!stack.peek().equals("(")) {
+                    String popped = stack.pop();
+                    if (popped.equals("+")) {
+                        n += k;
+                    } else if (popped.equals("-")) {
+                        n -= k;
+                    } else {
+                        k = Integer.valueOf(popped);
+                        if (stack.peek().equals("(")) {
+                            n += k;
+                        }
+                    }
+                }
+                stack.pop();
+                stack.push(n.toString());
+            }
+        }
+        stack.push(num.toString());
+        int k = 0;
+        while (!stack.isEmpty()) {
+            String popped = stack.pop();
+            if (popped.equals("+")) {
+                ans += k;
+            } else if (popped.equals("-")) {
+                ans -= k;
+            } else {
+                k = Integer.valueOf(popped);
+                if (stack.isEmpty()) {
+                    ans += k;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/basic-calculator/solutions/62361/iterative-java-solution-with-stack/">Stack</a>
+     *
+     * @param s
+     * @return
+     */
+    public static int calculate2(String s) {
         int result = 0;
         int number = 0;
         int sign = 1;
@@ -48,5 +111,16 @@ public class BasicCalculator {
         }
         result += number * sign;
         return result;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/basic-calculator/solutions/62362/java-easy-version-to-understand/">Stack: keeps the most recent sum at the top of stack(by cdai)</a>
+     * TODO
+     *
+     * @param s
+     * @return
+     */
+    public static int calculate3(String s) {
+        return 0;
     }
 }
