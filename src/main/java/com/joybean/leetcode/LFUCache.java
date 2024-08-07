f6d0cd25f49a50dd7bbf49c956b877400f1be034
joybeanx <joybeanq@gmail.com>
2024-08-07 16:36:51 +0800

    New solution for LFU Cache

--- src/main/java/com/joybean/leetcode/LFUCache.java
+++ src/main/java/com/joybean/leetcode/LFUCache.java
@@ -1,8 +1,6 @@
 package com.joybean.leetcode;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <a href="https://leetcode.com/problems/lfu-cache/">LFU Cache</a>
@@ -10,37 +8,97 @@ import java.util.Map;
  * @author Joybean
  */
 public class LFUCache {
-    private Map<Integer, Integer> keyToValue = new HashMap<>();
-    private Map<Integer, Integer> keyToFreq = new HashMap<>();
-    private Map<Integer, LinkedHashSet<Integer>> freqToKeys = new HashMap<>();
-    private int minFreq = 0;
     private int capacity;
+    Map<Integer, Integer> values = new HashMap<>();
+    Map<Integer, Integer> keyFrequencies = new HashMap<>();
+    Map<Integer, List<Integer>> frequencyKeys = new HashMap<>();
+    PriorityQueue<Integer> minFreqHeap = new PriorityQueue<>();
+
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
     }
 
     /**
-     * <a href="https://github.com/model-nut/algo-leetcode-labuladong-2020/blob/master/labuladong-algo-202012/%E6%95%B0%E6%8D
-     * %AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/LFU.md">from labuladong</a>
+     * Two HashMaps: use PriorityQueue keeps track of min frequency
      *
      * @param key
      * @return
      */
     public int get1(int key) {
-        if (!keyToValue.containsKey(key)) {
-            return -1;
+        if (values.containsKey(key)) {
+            increaseFreq1(key);
+            return values.get(key);
         }
-        increaseFreq(key);
-        return keyToValue.get(key);
+        return -1;
     }
 
     public void put1(int key, int value) {
-        if (capacity == 0) {
-            return;
+        if (values.containsKey(key)) {
+            increaseFreq1(key);
+            values.put(key, value);
+        } else {
+            if (values.size() == capacity) {
+                Integer minFreq = minFreqHeap.peek();
+                List<Integer> minFreqKeys = frequencyKeys.get(minFreq);
+                Integer minFreqKey = minFreqKeys.remove(0);
+                if (minFreqKeys.isEmpty()) {
+                    frequencyKeys.remove(minFreq);
+                }
+                keyFrequencies.remove(minFreqKey);
+                values.remove(minFreqKey);
+                if (minFreqKeys.isEmpty()) {
+                    minFreqHeap.poll();
+                }
+            }
+            values.put(key, value);
+            increaseFreq1(key);
         }
+    }
+
+    private void increaseFreq1(Integer key) {
+        int frequency = keyFrequencies.getOrDefault(key, 0);
+        int newFrequency = frequency + 1;
+        keyFrequencies.put(key, newFrequency);
+        if (frequencyKeys.containsKey(frequency)) {
+            List<Integer> keys = frequencyKeys.get(frequency);
+            keys.remove(key);
+            if (keys.isEmpty()) {
+                frequencyKeys.remove(frequency);
+                minFreqHeap.remove(frequency);
+            }
+        }
+        if (!frequencyKeys.containsKey(newFrequency)) {
+            minFreqHeap.offer(newFrequency);
+        }
+        frequencyKeys.putIfAbsent(newFrequency, new ArrayList<>());
+        frequencyKeys.get(newFrequency).add(key);
+    }
+
+
+    private Map<Integer, Integer> keyToValue = new HashMap<>();
+    private Map<Integer, Integer> keyToFreq = new HashMap<>();
+    private Map<Integer, LinkedHashSet<Integer>> freqToKeys = new HashMap<>();
+    private int minFreq = 0;
+
+    /**
+     * <a href="https://github.com/model-nut/algo-leetcode-labuladong-2020/blob/master/labuladong-algo-202012/%E6%95%B0%E6%8D
+     * %AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/LFU.md">Two HashMaps (by labuladong)</a>
+     *
+     * @param key
+     * @return
+     */
+    public int get2(int key) {
+        if (!keyToValue.containsKey(key)) {
+            return -1;
+        }
+        increaseFreq2(key);
+        return keyToValue.get(key);
+    }
+
+    public void put2(int key, int value) {
         if (keyToValue.containsKey(key)) {
-            increaseFreq(key);
+            increaseFreq2(key);
         } else {
             if (keyToValue.size() == capacity) {
                 removeLFUKey();
@@ -63,7 +121,7 @@ public class LFUCache {
         keyToValue.remove(lfuKey);
     }
 
-    private void increaseFreq(int key) {
+    private void increaseFreq2(int key) {
         int freq = keyToFreq.get(key);
         LinkedHashSet<Integer> keys = freqToKeys.get(freq);
         keys.remove(key);
@@ -79,19 +137,19 @@ public class LFUCache {
         freqToKeys.get(newFreq).add(key);
     }
 
+
     /**
-     * < a href="https://leetcode.com/problems/lfu-cache/discuss/207673/Python-concise-solution-**detailed
-     * **-explanation%3A-Two-dict-%2B-Doubly-linked-list">Doubly-linked list</a>
+     * <a href="https://leetcode.com/problems/lfu-cache/solutions/207673/python-concise-solution-detailed-explanation-two-dict-doubly-linked-list/">Custom doubly linked list (by mullen00)</a>
      * TODO
      *
      * @param key
      * @return
      */
-    public int get2(int key) {
+    public int get3(int key) {
         return 0;
     }
 
-    public void put2(int key, int value) {
-
+    public void put3(int key, int value) {
     }
+
 }
