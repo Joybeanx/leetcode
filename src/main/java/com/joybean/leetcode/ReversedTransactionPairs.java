759cf57614911e14c12cb83c59e43e2eaecea873
joybeanx <joybeanq@gmail.com>
2024-08-09 23:58:26 +0800

    Add Reversed Transaction Pairs

---
+++ src/main/java/com/joybean/leetcode/ReversedTransactionPairs.java
@@ -0,0 +1,19 @@
+package com.joybean.leetcode;
+
+/**
+ * <a href="https://leetcode.cn/problems/shu-zu-zhong-de-ni-xu-dui-lcof/">Reversed Transaction Pairs</a>
+ *
+ * @author Joybean
+ */
+public class ReversedTransactionPairs {
+    /**
+     * <a href="https://leetcode.cn/problems/shu-zu-zhong-de-ni-xu-dui-lcof/solutions/216984/shu-zu-zhong-de-ni-xu-dui-by-leetcode-solution/">Merge Sort</a>
+     * TODO
+     *
+     * @param record
+     * @return
+     */
+    public int reversePairs(int[] record) {
+        return 0;
+    }
+}
