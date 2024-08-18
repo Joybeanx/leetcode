17bddb6a958c6c63e0cf47d3daf2a75a5116645a
joybeanx <joybeanq@gmail.com>
2024-08-18 12:22:12 +0800

    Add todo

--- src/main/java/com/joybean/leetcode/MedianOfTwoSortedArrays.java
+++ src/main/java/com/joybean/leetcode/MedianOfTwoSortedArrays.java
@@ -42,4 +42,28 @@ public class MedianOfTwoSortedArrays {
         }
         throw new IllegalArgumentException();
     }
+
+    /**
+     * <a href="https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/2471/very-concise-o-log-min-m-n-iterative-solution-with-detailed-explanation/">Find a cut position that makes nums[L1]<nums[R2] and nums[L2]<nums[R1]</a>
+     * TODO
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays2(nums2, nums1);
+        }
+        int left = 0;
+        int right = 2 * m;
+
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+
+        }
+        return 0;
+    }
 }
