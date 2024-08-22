d7eec20cb4fd7253102a47dc16f7a2592ba6df0c
joybeanx <joybeanq@gmail.com>
2024-08-22 14:51:59 +0800

    New solutions for Spiral Matrix

--- src/main/java/com/joybean/leetcode/SpiralMatrix.java
+++ src/main/java/com/joybean/leetcode/SpiralMatrix.java
@@ -9,6 +9,41 @@ import java.util.List;
  * @author Joybean
  */
 public class SpiralMatrix {
+    /*
+    //Time Limit Exceeded, dead loop occurs due to traverse num multiple times
+    //case: [[1,2,3,4],[5,6,7,8],[9,10,11,12]], will add duplicate 6 at "ans.add(matrix[bottom][j])"
+    public static List<Integer> spiralOrder1(int[][] matrix) {
+         int m = matrix.length;
+        int n = matrix[0].length;
+        int startRow = 0;
+        int endRow = m - 1;
+        int startCol = 0;
+        int endCol = n - 1;
+
+        List<Integer> ans = new ArrayList<>(m * n);
+        while (ans.size() != m * n) {
+            for (int col = startCol; col <= endCol; col++) {
+                ans.add(matrix[startRow][col]);
+            }
+            startRow++;
+            for (int row = startRow; row <= endRow; row++) {
+                ans.add(matrix[row][endCol]);
+            }
+            endCol--;
+            for (int col = endCol; col >= startCol; col--) {
+                ans.add(matrix[endRow][col]);
+            }
+            endRow--;
+            for (int row = endRow; row >= startRow; row--) {
+                ans.add(matrix[row][startCol]);
+            }
+            startCol++;
+        }
+        return ans;
+    }
+    */
+
+
     /**
      * @param matrix
      * @return
@@ -44,13 +79,94 @@ public class SpiralMatrix {
     }
 
     /**
-     * <a href="https://leetcode.com/problems/spiral-matrix/solutions/20599/super-simple-and-easy-to-understand-solution/"> (by GreatLim)</a>
-     * TODO
+     * Traverse four directions and shrink [startRow,endRow] and [startCol,endCol]
      *
      * @param matrix
      * @return
      */
     public static List<Integer> spiralOrder2(int[][] matrix) {
-        return null;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int startRow = 0;
+        int endRow = m - 1;
+        int startCol = 0;
+        int endCol = n - 1;
+
+        List<Integer> ans = new ArrayList<>(m * n);
+        while (ans.size() != m * n) {
+            for (int col = startCol; col <= endCol; col++) {
+                ans.add(matrix[startRow][col]);
+            }
+            if (ans.size() == m * n) {
+                break;
+            }
+            startRow++;
+            for (int row = startRow; row <= endRow; row++) {
+                ans.add(matrix[row][endCol]);
+            }
+            if (ans.size() == m * n) {
+                break;
+            }
+            endCol--;
+            for (int col = endCol; col >= startCol; col--) {
+                ans.add(matrix[endRow][col]);
+            }
+            if (ans.size() == m * n) {
+                break;
+            }
+            endRow--;
+            for (int row = endRow; row >= startRow; row--) {
+                ans.add(matrix[row][startCol]);
+            }
+            startCol++;
+        }
+        return ans;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/spiral-matrix/solutions/20599/super-simple-and-easy-to-understand-solution/">Change direction and shrink [startRow,endRow] and [startCol,endCol] (by nhunghgnguyen)</a>
+     *
+     * @param matrix
+     * @return
+     */
+    public static List<Integer> spiralOrder3(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int startRow = 0;
+        int endRow = m - 1;
+        int startCol = 0;
+        int endCol = n - 1;
+        int dir = 0;
+        List<Integer> ans = new ArrayList<>(m * n);
+        while (startRow <= endRow && startCol <= endCol) {
+            switch (dir) {
+                case 0:
+                    for (int col = startCol; col <= endCol; col++) {
+                        ans.add(matrix[startRow][col]);
+                    }
+                    startRow++;
+                    break;
+                case 1:
+                    for (int row = startRow; row <= endRow; row++) {
+                        ans.add(matrix[row][endCol]);
+                    }
+                    endCol--;
+                    break;
+                case 2:
+                    for (int col = endCol; col >= startCol; col--) {
+                        ans.add(matrix[endRow][col]);
+                    }
+                    endRow--;
+                    break;
+                case 3:
+                    for (int row = endRow; row >= startRow; row--) {
+                        ans.add(matrix[row][startCol]);
+                    }
+                    startCol++;
+                    break;
+            }
+            dir = (dir + 1) % 4;
+        }
+        return ans;
     }
 }
