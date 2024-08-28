41cb47be7eb047a1120d4afbd0f34f8697fea5c5
joybeanx <joybeanq@gmail.com>
2024-08-28 14:56:14 +0800

    New Newton's method for Sqrt(x)

--- src/main/java/com/joybean/leetcode/SqrtX.java
+++ src/main/java/com/joybean/leetcode/SqrtX.java
@@ -6,26 +6,78 @@ package com.joybean.leetcode;
  * @author Joybean
  */
 public class SqrtX {
+
+    /**
+     * Linear search 1
+     *
+     * @param x
+     * @return
+     * @see <a href="https://en.wikipedia.org/wiki/Integer_square_root">Integer Square Root</a>
+     */
+    public static int mySqrt1(int x) {
+        //use long to avoid integer overflow when calculating i*i
+        //case: x = 2147395600,  463460 * 463460 = 2147395600, 463460 * 46341 = -2147479015
+        long i = 0;
+        while (i * i <= x) {
+            i++;
+        }
+        return (int) (i - 1);
+    }
+
+    /**
+     * Linear search 2
+     *
+     * @param x
+     * @return
+     * @see <a href="https://en.wikipedia.org/wiki/Integer_square_root">Integer Square Root</a>
+     */
+    public static int mySqrt2(int x) {
+        //use long to avoid integer overflow when calculating i*i
+        //case: x = 2147395600,  463460 * 463460 = 2147395600, 463460 * 46341 = -2147479015
+        long i = 0;
+        while ((i + 1) * (i + 1) <= x) {
+            i++;
+        }
+        return (int) i;
+    }
+
+   /*
+    //wrong solution, overflow occurs when x = Integer.MAX_VALUE
+    public static int mySqrt(int x) {
+        int left = 0;
+        int right = x + 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            long p = (long)mid * mid;
+            if (p > x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left - 1;
+    }
+    */
+
     /**
      * Binary search 1
      *
      * @param x
      * @return
      */
-    public static int mySqrt1(int x) {
+    public static int mySqrt3(int x) {
         int left = 0;
         int right = x;
         //search range [left,right)
         while (left < right) {
             int mid = (left + right + 1) >>> 1;
-            int cur = x / mid;
-            if (mid == cur) {
-                return mid;
-            }
-            if (mid > cur) {
+            int r = x / mid;
+            if (r > mid) {
+                left = mid;
+            } else if (r < mid) {
                 right = mid - 1;
             } else {
-                left = mid;
+                return mid;
             }
         }
         return left;
@@ -37,7 +89,7 @@ public class SqrtX {
      * @param x
      * @return
      */
-    public static int mySqrt2(int x) {
+    public static int mySqrt4(int x) {
         int left = 1;
         int right = x;
         int ans = 0;
@@ -52,5 +104,19 @@ public class SqrtX {
             }
         }
         return ans;
+    }
+
+    /**
+     * <a href="https://en.wikipedia.org/wiki/Newton%27s_method">Newton's method</a>
+     *
+     * @param x
+     * @return
+     */
+    public static int mySqrtx5(int x) {
+        if (x == 0) return 0;
+        long i = x;
+        while (i > x / i)
+            i = (i + x / i) / 2;
+        return (int) i;
     }
 }
